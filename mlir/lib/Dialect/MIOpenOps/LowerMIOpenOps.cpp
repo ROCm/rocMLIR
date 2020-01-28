e587a8a23c9812610dd1e79a3e1211e1f4d8aba5
Wen-Heng (Jack) Chung <whchung@gmail.com>
2020-01-28 22:09:12 +0000

    Inital commit to add MIOpen Conv2D to Transform and GridwiseGemm transform pass.

---
+++ mlir/lib/Dialect/MIOpenOps/LowerMIOpenOps.cpp
@@ -0,0 +1,82 @@
+//===- LowerMIOpenOps.cpp - MLIR MIOpen ops lowering passes ---------------===//
+//
+// Copyright 2020 The MLIR Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// =============================================================================
+//
+// This pass converts miopen.conv2d into miopen.transform and
+// miopen.gridwise_gemm.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Dialect/MIOpenOps/MIOpenOps.h"
+#include "mlir/Dialect/MIOpenOps/Passes.h"
+#include "mlir/Dialect/StandardOps/Ops.h"
+#include "mlir/IR/Attributes.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/IR/Module.h"
+#include "mlir/IR/Operation.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/IR/StandardTypes.h"
+#include "mlir/IR/Types.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Pass/PassRegistry.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "mlir/Transforms/Passes.h"
+#include "mlir/Support/LogicalResult.h"
+
+using namespace mlir;
+
+struct Conv2DOpRewritePattern : public OpRewritePattern<miopen::Conv2DOp> {
+  using OpRewritePattern<miopen::Conv2DOp>::OpRewritePattern;
+
+  PatternMatchResult
+  matchAndRewrite(miopen::Conv2DOp op, PatternRewriter &rewriter) const override {
+    rewriter.create<miopen::TransformOp>(op.getLoc(), op.filter().getType(), op.filter());
+
+    rewriter.create<miopen::TransformOp>(op.getLoc(), op.input().getType(), op.input());
+    rewriter.create<miopen::TransformOp>(op.getLoc(), op.input().getType(), op.input());
+    rewriter.create<miopen::TransformOp>(op.getLoc(), op.input().getType(), op.input());
+
+    rewriter.create<miopen::TransformOp>(op.getLoc(), op.output().getType(), op.output());
+
+    //rewriter.create<miopen::GridwiseGemmOp>(op.getLoc(), op.filter(), op.input(), op.output());
+
+    // Finally, erase the original Conv2D op.
+    op.erase();
+
+    return matchSuccess();
+  }
+};
+
+namespace {
+struct LowerMIOpenOpsPass : public ModulePass<LowerMIOpenOpsPass> {
+  void runOnModule() override;
+};
+} // end anonymous namespace
+
+void LowerMIOpenOpsPass::runOnModule() {
+  OwningRewritePatternList patterns;
+  patterns.insert<Conv2DOpRewritePattern>(&getContext());
+  applyPatternsGreedily(getModule(), patterns);
+}
+
+std::unique_ptr<OpPassBase<ModuleOp>> mlir::miopen::createLowerMIOpenOpsPass() {
+  return std::make_unique<LowerMIOpenOpsPass>();
+}
+
+static PassRegistration<LowerMIOpenOpsPass>
+    lowerMIOpenOpsPass("miopen-lowering",
+                       "Lower MIOpen conv2d into transform and gridwise_gemm.");
