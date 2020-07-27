0afc604c8dad4d7ac8f6b3f3d69a11e42e146df9
Wen-Heng (Jack) Chung <whchung@gmail.com>
2020-07-27 13:05:29 -0500

    Skeleton of miopen.xdlops_gemm lowering.

--- mlir/lib/Dialect/MIOpen/Transforms/LowerMIOpenOps.cpp
+++ mlir/lib/Dialect/MIOpen/Transforms/LowerMIOpenOps.cpp
@@ -138,6 +138,7 @@ void LowerMIOpenOpsStep4Pass::runOnOperation() {
   OwningRewritePatternList patterns;
   patterns.insert<ThreadwiseGemmRewritePattern>(&getContext());
   patterns.insert<ThreadwiseCopyRewritePattern>(&getContext());
+  patterns.insert<XdlopsGemmRewritePattern>(&getContext());
   applyPatternsAndFoldGreedily(getOperation(), patterns);
 }
 
