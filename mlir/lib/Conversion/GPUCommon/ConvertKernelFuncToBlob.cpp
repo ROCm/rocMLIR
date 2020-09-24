2860ad870b96109c6cea060f8047d5948b47027b
Wen-Heng (Jack) Chung <whchung@gmail.com>
2020-09-24 00:07:54 +0000

    XXX use -O0 when generating ISA for GPU kernels.

--- mlir/lib/Conversion/GPUCommon/ConvertKernelFuncToBlob.cpp
+++ mlir/lib/Conversion/GPUCommon/ConvertKernelFuncToBlob.cpp
@@ -141,6 +141,8 @@ OwnedBlob GpuKernelToBlobPass::convertModuleToBlob(llvm::Module &llvmModule,
                                                     features, {}, {}));
   }
 
+  // XXX. Do NOT use optimizations for now.
+  targetMachine->setOptLevel(llvm::CodeGenOpt::None);
   llvmModule.setDataLayout(targetMachine->createDataLayout());
 
   auto targetISA = translateModuleToISA(llvmModule, *targetMachine);
