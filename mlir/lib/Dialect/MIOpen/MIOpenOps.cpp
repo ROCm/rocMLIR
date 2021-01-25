dbce715e7938a4f0259978be2d8220f8dbc58336
Wen-Heng (Jack) Chung <whchung@gmail.com>
2021-01-25 15:06:20 +0000

    Introduce miopen.threadwise_load op.

--- mlir/lib/Dialect/MIOpen/MIOpenOps.cpp
+++ mlir/lib/Dialect/MIOpen/MIOpenOps.cpp
@@ -599,6 +599,37 @@ static LogicalResult verify(ThreadwiseCopyOp op) {
 }
 
 //===----------------------------------------------------------------------===//
+// ThreadwiseLoadOp
+//===----------------------------------------------------------------------===//
+
+static ParseResult parseThreadwiseLoadOp(OpAsmParser &parser, OperationState &result) {
+  SmallVector<OpAsmParser::OperandType, 6> ops;
+  SmallVector<Type, 2> types;
+
+  auto ret = parser.parseOperandList(ops, OpAsmParser::Delimiter::Paren) ||
+             parser.parseOptionalAttrDict(result.attributes) ||
+             parser.parseColonTypeList(types) ||
+             parser.resolveOperand(ops[0], types[0], result.operands) ||
+             parser.addTypeToList(types[1], result.types);
+
+  for (unsigned i = 1; i < ops.size(); ++i) {
+    ret &= succeeded(parser.resolveOperand(
+        ops[i], parser.getBuilder().getIntegerType(32), result.operands));
+  }
+  return failure(ret);
+}
+
+static void print(OpAsmPrinter &p, ThreadwiseLoadOp op) {
+  p << op.getOperationName() << "(" << op.getOperands() << ")";
+  p.printOptionalAttrDict(op.getAttrs());
+  p << " : " << op.getOperand(0).getType() << ", " << op.getType();
+}
+
+static LogicalResult verify(ThreadwiseLoadOp op) {
+  return success();
+}
+
+//===----------------------------------------------------------------------===//
 // ThreadwiseCopyV2Op
 //===----------------------------------------------------------------------===//
 
