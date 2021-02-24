07f86d53e8837d9b8ae441a2835e12b1900f98a0
SJW <swaters@amd.com>
2021-02-24 20:46:07 +0000

    Initial commit for miopen-project * Added submodule llvm-project in external dir   * Set commit to 1ac98044dfc250a0454762eb49f138b1f3f035e7 * Removed all llvm-project directories except mlir * Removed all unchanged files from mlir (except some test directory files)

--- compiler-rt/test/sanitizer_common/ios_commands/iossim_prepare.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-
-import json
-
-print(json.dumps({"env": {}}))
--- flang/test/Semantics/if_stmt02.f90
+++
@@ -1,4 +0,0 @@
-! RUN: %S/test_errors.sh %s %t %f18
-!ERROR: IF statement is not allowed in IF statement
-IF (A > 0.0) IF (B < 0.0) A = LOG (A)
-END
