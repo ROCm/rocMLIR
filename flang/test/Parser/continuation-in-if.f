2ca89c4a0f29f817aefe279590b405a677d58c99
SJW <swaters@amd.com>
2021-02-24 20:46:07 +0000

    Initial commit for miopen-project * Added submodule llvm-project in external dir   * Set commit to 1ac98044dfc250a0454762eb49f138b1f3f035e7 * Removed all llvm-project directories except mlir * Removed all unchanged files from mlir (except some test directory files)

--- flang/test/Parser/continuation-in-if.f
+++
@@ -1,9 +0,0 @@
-! RUN: %f18 -funparse %s 2>&1 | FileCheck %s
-! CHECK: CALL foo("N","N")
-#ifdef transpose
-      call foo('T',
-#else
-      call foo('N',
-#endif
-     $     'N')
-      end
--- flang/test/Semantics/if_arith01.f90
+++
@@ -1,8 +0,0 @@
-! RUN: %S/test_errors.sh %s %t %f18
-! Check that a basic arithmetic if compiles.
-
-if ( A ) 100, 200, 300
-100 CONTINUE
-200 CONTINUE
-300 CONTINUE
-END
