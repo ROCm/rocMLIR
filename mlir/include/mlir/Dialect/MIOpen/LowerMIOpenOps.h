59949200b6134e8268197e890ae8fe26b712671c
hanchang <hanwen.chang@amd.com>
2021-03-16 14:34:15 +0000

    fix non xdlops step3 failed

--- mlir/include/mlir/Dialect/MIOpen/LowerMIOpenOps.h
+++ mlir/include/mlir/Dialect/MIOpen/LowerMIOpenOps.h
@@ -3617,9 +3617,6 @@ struct ThreadwiseGemmRewritePattern
     ArrayRef<int64_t> gemmBShape =
         gemmB.getType().dyn_cast<MemRefType>().getShape();
 
-    auto destType = gemmC.getType().cast<MemRefType>();
-    auto dataType = destType.getElementType();
-
     auto loopK = b.create<AffineForOp>(loc, 0, gemmAShape[0], 1);
     auto lbK = loopK.getBody();
     b.setInsertionPointToStart(lbK);
@@ -3639,16 +3636,17 @@ struct ThreadwiseGemmRewritePattern
     SmallVector<Value, 2> memIndicesKN;
     extractForInductionVars({loopK, loopN}, &memIndicesKN);
     auto gemmBKN = b.create<AffineLoadOp>(loc, gemmB, memIndicesKN);
-    auto mul = b.create<MulFOp>(loc, dataType, gemmAKM, gemmBKN);
 
-    Value mul = b.create<MulFOp>(loc, b.getF32Type(), gemmAKM, gemmBKN);
-    if (dataType == b.getIntegerType(16))
-      mul = b.create<MulIOp>(loc, b.getIntegerType(16), gemmAKM, gemmBKN);
+    Value mul = b.create<MulFOp>(loc, dataType, gemmAKM, gemmBKN);
+    if (dataType.isa<IntegerType>())
+      mul = b.create<MulIOp>(loc, dataType, gemmAKM, gemmBKN);
     SmallVector<Value, 2> memIndicesMN;
     extractForInductionVars({loopM, loopN}, &memIndicesMN);
     auto gemmCMN = b.create<AffineLoadOp>(loc, gemmC, memIndicesMN);
 
-    auto add = b.create<AddFOp>(loc, dataType, mul, gemmCMN);
+    Value add = b.create<AddFOp>(loc, dataType, mul, gemmCMN);
+    if (dataType.isa<IntegerType>())
+      add = b.create<AddIOp>(loc, dataType, mul, gemmCMN);
     auto store = b.create<AffineStoreOp>(loc, add, gemmC, memIndicesMN);
 
     op.erase();
