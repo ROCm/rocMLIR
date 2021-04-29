a0f2c05cbdeedf59be50321fc8500892e0a8efbb
Jeffrey Poznanovic <jeffrey.poznanovic@amd.com>
2021-04-29 15:02:51 -0500

    Partitioned files into separate mlir/ and mlir-patch/ subdirs.  Revamped and pruned the CMake build system and src-level files.

--- cmake/mlir.cmake
+++ cmake/mlir.cmake
@@ -1,5 +1,21 @@
 message(STATUS "Adding LLVM git-submodule src dependency")
 
+# Forbid implicit function declaration: this may lead to subtle bugs and we
+# don't have a reason to support this.
+check_c_compiler_flag("-Werror=implicit-function-declaration" C_SUPPORTS_WERROR_IMPLICIT_FUNCTION_DECLARATION)
+append_if(C_SUPPORTS_WERROR_IMPLICIT_FUNCTION_DECLARATION "-Werror=implicit-function-declaration" CMAKE_C_FLAGS)
+
+# Build the ROCm conversions and run according tests if the AMDGPU backend
+# is available
+if ("AMDGPU" IN_LIST LLVM_TARGETS_TO_BUILD)
+  set(MLIR_ROCM_CONVERSIONS_ENABLED 1)
+else()
+  set(MLIR_ROCM_CONVERSIONS_ENABLED 0)
+endif()
+add_definitions(-DMLIR_ROCM_CONVERSIONS_ENABLED=${MLIR_ROCM_CONVERSIONS_ENABLED})
+set(MLIR_ROCM_RUNNER_ENABLED 0 CACHE BOOL "Enable building the mlir ROCm runner")
+
+# LLVM settings
 set(LLVM_ENABLE_PROJECTS "mlir;lld" CACHE INTERNAL "")
 set(LLVM_BUILD_EXAMPLES ON CACHE INTERNAL "")
 set(LLVM_TARGETS_TO_BUILD "X86;AMDGPU" CACHE INTERNAL "")
@@ -7,15 +23,12 @@ set(CMAKE_BUILD_TYPE Release CACHE INTERNAL "")
 set(LLVM_ENABLE_ASSERTIONS ON CACHE INTERNAL "")
 set(BUILD_SHARED_LIBS ON CACHE INTERNAL "")
 set(LLVM_BUILD_LLVM_DYLIB ON CACHE INTERNAL "")
-#set(MLIR_ROCM_RUNNER_ENABLED 1 CACHE INTERNAL "")
-#set(MLIR_MIOPEN_DRIVER_ENABLED 1 CACHE INTERNAL "")
-#set(MLIR_ENABLE_SQLITE 1 CACHE INTERNAL "")
 set(LLVM_INSTALL_UTILS ON CACHE INTERNAL "")
-
-# Library type and linkage settings
-#set(LLVM_BUILD_LLVM_DYLIB OFF CACHE INTERNAL "")
-#set(LLVM_LINK_LLVM_DYLIB  OFF CACHE INTERNAL "")
-#set(BUILD_SHARED_LIBS OFF CACHE INTERNAL "")
+set(LLVM_ENABLE_PROJECTS "mlir;lld" CACHE STRING "List of default llvm targets")
+set(LLVM_TARGETS_TO_BUILD "X86;AMDGPU" CACHE STRING "")
+set(LLVM_ENABLE_TERMINFO OFF CACHE BOOL "")
+set(LLVM_ENABLE_ASSERTIONS ON CACHE BOOL "")
+set(MLIR_ENABLE_SQLITE ON CACHE BOOL "")
 
 set(MLIR_TABLEGEN_EXE mlir-tblgen)
 set(LLVM_PROJ_SRC "${CMAKE_SOURCE_DIR}/external/llvm-project")
