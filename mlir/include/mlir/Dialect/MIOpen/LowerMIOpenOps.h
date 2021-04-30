f9b59b1336ba4110e0396dd45fcc2790560f2004
letaoqin <letaoqin@amd.com>
2021-04-30 16:06:26 +0800

    move math function to single file

--- mlir/include/mlir/Dialect/MIOpen/LowerMIOpenOps.h
+++ mlir/include/mlir/Dialect/MIOpen/LowerMIOpenOps.h
@@ -39,50 +39,12 @@
 
 #include "XdlopsCodeSelection.h"
 #include "mlir/Dialect/MIOpen/Tuning/GridwiseGemmParams.h"
+#include "utility/math.hpp"
 #include "llvm/ADT/SmallVector.h"
 
 using namespace mlir;
 using namespace mlir::miopen;
 
-namespace math {
-
-// greatest common divisor, aka highest common factor
-template <typename T> T gcd(T x, T y) {
-  if (x == y || x == 0) {
-    return y;
-  } else if (y == 0) {
-    return x;
-  } else if (x > y) {
-    return gcd(x - y, y);
-  } else {
-    return gcd(x, y - x);
-  }
-}
-
-template <typename X, typename... Ys> auto gcd(X x, Ys... ys) {
-  return gcd(x, ys...);
-}
-
-// least common multiple
-template <typename T> T lcm(T x, T y) { return (x * y) / gcd(x, y); }
-
-template <typename X, typename... Ys> auto lcm(X x, Ys... ys) {
-  return lcm(x, lcm(ys...));
-}
-
-template <class X, class Y> auto integer_divide_ceil(X x, Y y) {
-  return (x + y - 1) / y;
-}
-
-template <class X, class Y> constexpr auto integer_divide_floor(X x, Y y) {
-  return x / y;
-}
-
-template <class X, class Y> auto integer_least_multiple(X x, Y y) {
-  return y * integer_divide_ceil(x, y);
-}
-
-} // namespace math
 //===----------------------------------------------------------------------===//
 // Utility function to emit constant float op.
 //===----------------------------------------------------------------------===//
