140429d88a754a83a3c7cd908a30f3b20cf7eb2f
jungpark-mlir <jungwook.park@amd.com>
2021-06-24 15:46:56 +0100

    Update MIGraphXOps.cpp
    fix print out

--- mlir/lib/Dialect/MIGraphX/MIGraphXOps.cpp
+++ mlir/lib/Dialect/MIGraphX/MIGraphXOps.cpp
@@ -256,6 +256,7 @@ static ParseResult parseConstantOp(OpAsmParser &parser, OperationState &result) 
 }
 
 static void print(OpAsmPrinter &p, migraphx::ConstantOp op) {
+  p << op.getOperationName() << "(" << op.getOperands() << ")";
   p.printOptionalAttrDict(op.getAttrs());
 }
 
