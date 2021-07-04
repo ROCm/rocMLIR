d315eb9f44a6bac926f6e4143a32af47f643216f
jungpark-mlir <jungwook.park@amd.com>
2021-07-04 15:41:16 +0100

    Update TosaToMIGraphX.cpp

--- mlir/lib/Conversion/TosaToMIGraphX/TosaToMIGraphX.cpp
+++ mlir/lib/Conversion/TosaToMIGraphX/TosaToMIGraphX.cpp
@@ -10,6 +10,8 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "mlir/Dialect/MIGraphX/MIGraphXOps.h"
+#include "mlir/Dialect/Tosa/IR/TosaOps.h"
 #include "mlir/Dialect/StandardOps/IR/Ops.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Transforms/DialectConversion.h"
