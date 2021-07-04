e4db9b452a81e2aba83fdcc0ad81198c6705f127
jungpark-mlir <jungwook.park@amd.com>
2021-07-04 15:25:24 +0100

    Update TosaToMIGraphXPass.cpp

--- mlir/lib/Conversion/TosaToMIGraphX/TosaToMIGraphXPass.cpp
+++ mlir/lib/Conversion/TosaToMIGraphX/TosaToMIGraphXPass.cpp
@@ -64,9 +64,8 @@ public:
     target.markUnknownOpDynamicallyLegal([](Operation *) { return true; });
 
     FuncOp func = getFunction();
-    //mlir::tosa::populateWithGenerated(func.getContext(), patterns);
-    populateWithGenerated(func.getContext(), patterns);
-
+    mlir::tosa::populateWithGenerated(func.getContext(), patterns);
+   
     if (failed(applyFullConversion(func, target, std::move(patterns)))) {
       signalPassFailure();
     }
