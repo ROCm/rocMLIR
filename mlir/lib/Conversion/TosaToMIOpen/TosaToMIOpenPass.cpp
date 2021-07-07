8b1fb37e2571b034bd36d1025c85a60822c43be4
SJW <48454132+sjw36@users.noreply.github.com>
2021-07-07 16:29:55 -0500

    Conversion path for TOSA-to-MIOpen (#210)
    * MLIR#101: Added tosa-to-miopen-on-tensors conversion (remove the on-tensors)
    * This bufferizes for the call to miopen::conv2d
    
    * * removed unused miopen structured ops
    * set attributes for the miopen::conv2d (hard-coded)
    
    * * connect padding, stride, dilation
    
    * * updated for 5-d mlir::conv2d
      * generate reshape op
    * updated for 4-d padding
    
    * * test --tosa-to-miopen-on-tensors
    
    * MLIR#101: Added tosa-to-miopen-on-tensors conversion (remove the on-tensors)
    * This bufferizes for the call to miopen::conv2d
    
    * * removed unused miopen structured ops
    * set attributes for the miopen::conv2d (hard-coded)
    
    * * connect padding, stride, dilation
    
    * * updated for 5-d mlir::conv2d
      * generate reshape op
    * updated for 4-d padding
    
    * * cleanup
    
    * * renamed to drop -on-tensors
    
    * * fixed segfault

---
+++ mlir/lib/Conversion/TosaToMIOpen/TosaToMIOpenPass.cpp
@@ -0,0 +1,61 @@
+//===- TosaToMIOpenPass.cpp - Lowering Tosa to MIOpen Dialect -------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This transformation pass legalizes Tosa operations to the MIOpen dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#include "../PassDetail.h"
+#include "mlir/Conversion/TosaToMIOpen/TosaToMIOpen.h"
+#include "mlir/Dialect/Linalg/IR/LinalgOps.h"
+#include "mlir/Dialect/MIOpen/MIOpenOps.h"
+#include "mlir/Dialect/StandardOps/IR/Ops.h"
+#include "mlir/Dialect/Tosa/IR/TosaOps.h"
+#include "mlir/Dialect/Tosa/Transforms/PassDetail.h"
+#include "mlir/Dialect/Tosa/Transforms/Passes.h"
+#include "mlir/Dialect/Tosa/Utils/QuantUtils.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+
+using namespace mlir;
+
+namespace {
+struct TosaToMIOpen
+    : public TosaToMIOpenBase<TosaToMIOpen> {
+public:
+  void getDependentDialects(DialectRegistry &registry) const override {
+    registry.insert<miopen::MIOpenDialect, linalg::LinalgDialect,
+                    StandardOpsDialect>();
+  }
+
+  void runOnFunction() override {
+    OwningRewritePatternList patterns;
+    ConversionTarget target(getContext());
+    target.addLegalDialect<miopen::MIOpenDialect, linalg::LinalgDialect,
+                           StandardOpsDialect>();
+    target.addIllegalOp<tosa::Conv2DOp>();
+    target.markUnknownOpDynamicallyLegal([](Operation *) { return true; });
+
+    FuncOp func = getFunction();
+    mlir::tosa::populateTosaToMIOpenConversionPatterns(
+        func.getContext(), &patterns);
+    if (failed(applyFullConversion(func, target, std::move(patterns))))
+      signalPassFailure();
+  }
+};
+} // namespace
+
+std::unique_ptr<Pass> mlir::tosa::createTosaToMIOpen() {
+  return std::make_unique<TosaToMIOpen>();
+}
+
+void mlir::tosa::addTosaToMIOpenPasses(OpPassManager &pm) {
+  pm.addNestedPass<FuncOp>(createTosaToMIOpen());
+}
