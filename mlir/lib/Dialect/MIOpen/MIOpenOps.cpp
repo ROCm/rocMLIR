9dcd28c4e3fa7eb66f69dce0fd91d53cedacc186
Wen-Heng (Jack) Chung <whchung@gmail.com>
2021-08-10 17:26:40 -0500

    XXX WIP lowering logic of miopen-lowering-step2 for non-XDLOPS path.

--- mlir/lib/Dialect/MIOpen/MIOpenOps.cpp
+++ mlir/lib/Dialect/MIOpen/MIOpenOps.cpp
@@ -549,21 +549,19 @@ static ParseResult parseBlockwiseLoadOp(OpAsmParser &parser,
                                         OperationState &result) {
   SmallVector<OpAsmParser::OperandType, 2> ops;
   SmallVector<Type, 2> types;
-  VectorType coordVectorType =
-      VectorType::get(3, parser.getBuilder().getIntegerType(32));
   return failure(
       parser.parseOperandList(ops, OpAsmParser::Delimiter::Paren) ||
       parser.parseOptionalAttrDict(result.attributes) ||
       parser.parseColonTypeList(types) ||
       parser.resolveOperand(ops[0], types[0], result.operands) ||
-      parser.resolveOperand(ops[1], coordVectorType, result.operands) ||
-      parser.addTypeToList(types[1], result.types));
+      parser.resolveOperand(ops[1], types[1], result.operands) ||
+      parser.addTypeToList(types[2], result.types));
 }
 
 static void print(OpAsmPrinter &p, BlockwiseLoadOp op) {
   p << op.getOperationName() << "(" << op.getOperands() << ")";
   p.printOptionalAttrDict(op.getAttrs());
-  p << " : " << op.getOperand(0).getType() << ", " << op.getType();
+  p << " : " << op.getOperand(0).getType() << ", " << op.getOperand(1).getType() << ", " << op.getType();
 }
 
 static LogicalResult verify(BlockwiseLoadOp op) { return success(); }
@@ -576,22 +574,19 @@ static ParseResult parseBlockwiseStoreOp(OpAsmParser &parser,
                                          OperationState &result) {
   SmallVector<OpAsmParser::OperandType, 2> ops;
   SmallVector<Type, 2> types;
-  VectorType coordVectorType =
-      VectorType::get(3, parser.getBuilder().getIntegerType(32));
   return failure(
       parser.parseOperandList(ops, OpAsmParser::Delimiter::Paren) ||
       parser.parseOptionalAttrDict(result.attributes) ||
       parser.parseColonTypeList(types) ||
       parser.resolveOperand(ops[0], types[0], result.operands) ||
       parser.resolveOperand(ops[1], types[1], result.operands) ||
-      parser.resolveOperand(ops[2], coordVectorType, result.operands));
+      parser.resolveOperand(ops[2], types[2], result.operands));
 }
 
 static void print(OpAsmPrinter &p, BlockwiseStoreOp op) {
   p << op.getOperationName() << "(" << op.getOperands() << ")";
   p.printOptionalAttrDict(op.getAttrs());
-  p << " : " << op.getOperand(0).getType() << ", "
-    << op.getOperand(1).getType();
+  p << " : " << op.getOperand(0).getType() << ", " << op.getOperand(1).getType() << ", " << op.getOperand(2).getType();
 }
 
 static LogicalResult verify(BlockwiseStoreOp op) { return success(); }
