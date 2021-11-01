7049000ef086cbe5ab834c5704d676b7f9a4f92f
jungpark-mlir <jungwook.park@amd.com>
2021-11-01 13:47:38 +0000

    Update llvm-project.cmake

--- cmake/llvm-project.cmake
+++ cmake/llvm-project.cmake
@@ -53,8 +53,8 @@ list(APPEND CMAKE_EXE_LINKER_FLAGS
   " -Wl,-rpath -Wl,${CMAKE_CURRENT_BINARY_DIR}/external/llvm-project/llvm/lib"
 )
 
-if( BUILD_FAT_LIBMLIRMIOPEN AND (NOT MLIR_EXPORT_ALL))
-  add_subdirectory("${LLVM_PROJ_SRC}/llvm" "external/llvm-project/llvm" EXCLUDE_FROM_ALL)
-else()
+if(MLIR_EXPORT_ALL)
   add_subdirectory("${LLVM_PROJ_SRC}/llvm" "external/llvm-project/llvm")
+else()
+  add_subdirectory("${LLVM_PROJ_SRC}/llvm" "external/llvm-project/llvm" EXCLUDE_FROM_ALL)
 endif()
