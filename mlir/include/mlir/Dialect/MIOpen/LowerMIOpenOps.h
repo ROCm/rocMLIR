5380b6aab4f64662f22d4efd9ae510a4f4fdbdd3
Wen-Heng (Jack) Chung <whchung@gmail.com>
2021-12-02 01:11:13 +0000

    Fix clang-format.

--- mlir/include/mlir/Dialect/MIOpen/LowerMIOpenOps.h
+++ mlir/include/mlir/Dialect/MIOpen/LowerMIOpenOps.h
@@ -11066,8 +11066,8 @@ struct XdlopsGemmV2RewritePattern
       if (argTypeVectorLength > 1) {
         Value zeroOp = createZeroConstantFloatOp(innerLoopb, loc, dataType);
 
-        Value offset = innerLoopb.create<AddIOp>(loc,
-            innerLoopb.create<MulIOp>(loc, outerLoopiv, KBaseConstantOp),
+        Value offset = innerLoopb.create<AddIOp>(
+            loc, innerLoopb.create<MulIOp>(loc, outerLoopiv, KBaseConstantOp),
             innerLoopb.create<MulIOp>(loc, innerLoopiv, KBaseConstantOp));
         if (bufferAElementType.isa<VectorType>()) {
           // bufferA/BElement loaded on LDS are vectors.
