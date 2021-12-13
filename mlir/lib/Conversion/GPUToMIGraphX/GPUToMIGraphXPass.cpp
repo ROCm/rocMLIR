cce8d5093d42e5f2fa498076165e2b49c62af44a
jungpark-mlir <jungwook.park@amd.com>
2021-12-13 13:18:39 +0000

    Update GPUToMIGraphXPass.cpp

--- mlir/lib/Conversion/GPUToMIGraphX/GPUToMIGraphXPass.cpp
+++ mlir/lib/Conversion/GPUToMIGraphX/GPUToMIGraphXPass.cpp
@@ -52,7 +52,7 @@ public:
 
     ConversionTarget target(ctx);
     target.addLegalDialect<migraphx::MIGraphXDialect, StandardOpsDialect, gpu::GPUDialect, memref::MemRefDialect, LLVM::LLVMDialect>();
-    target.addIllegalOp<CallOp>();
+    //target.addIllegalOp<CallOp>();
 
     FuncOp func = getFunction();
     mlir::migraphx::populateFuncToCOBJPatterns(
