a70a3987a805ebabed2132b8aaae99b8348d9905
jungpark-mlir <jungwook.park@amd.com>
2021-12-14 16:17:56 +0000

    clang format fix (#457)

--- mlir/lib/Conversion/GPUToMIGraphX/GPUToMIGraphXPass.cpp
+++ mlir/lib/Conversion/GPUToMIGraphX/GPUToMIGraphXPass.cpp
@@ -1,4 +1,5 @@
-//===- GPUToMIGraphXPass.cpp - Lowering GPU to MIGraphX Dialect -------------===//
+//===- GPUToMIGraphXPass.cpp - Lowering GPU to MIGraphX Dialect
+//-------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -12,13 +13,13 @@
 
 #include "../PassDetail.h"
 #include "mlir/Conversion/GPUToMIGraphX/GPUToMIGraphX.h"
-#include "mlir/Dialect/StandardOps/IR/Ops.h"
-#include "mlir/Dialect/GPU/GPUDialect.h"
-#include "mlir/Dialect/MemRef/IR/MemRef.h"
-#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
 #include "mlir/Conversion/LLVMCommon/ConversionTarget.h"
 #include "mlir/Conversion/LLVMCommon/Pattern.h"
 #include "mlir/Conversion/MemRefToLLVM/MemRefToLLVM.h"
+#include "mlir/Dialect/GPU/GPUDialect.h"
+#include "mlir/Dialect/LLVMIR/LLVMDialect.h"
+#include "mlir/Dialect/MemRef/IR/MemRef.h"
+#include "mlir/Dialect/StandardOps/IR/Ops.h"
 
 #include "mlir/Conversion/AsyncToLLVM/AsyncToLLVM.h"
 #include "mlir/Conversion/StandardToLLVM/ConvertStandardToLLVM.h"
@@ -32,17 +33,17 @@
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 
 #include "mlir/Conversion/GPUToROCDL/GPUToROCDLPass.h"
-#include "mlir/ExecutionEngine/ROCm/BackendUitls.h"
 #include "mlir/ExecutionEngine/OptUtils.h"
-
+#include "mlir/ExecutionEngine/ROCm/BackendUitls.h"
 
 using namespace mlir;
 
-struct GPUToMIGraphX
-    : public GPUToMIGraphXBase<GPUToMIGraphX> {
+struct GPUToMIGraphX : public GPUToMIGraphXBase<GPUToMIGraphX> {
 public:
   void getDependentDialects(DialectRegistry &registry) const override {
-    registry.insert<migraphx::MIGraphXDialect, StandardOpsDialect, gpu::GPUDialect, memref::MemRefDialect, LLVM::LLVMDialect>();
+    registry
+        .insert<migraphx::MIGraphXDialect, StandardOpsDialect, gpu::GPUDialect,
+                memref::MemRefDialect, LLVM::LLVMDialect>();
   }
 
   void runOnFunction() override {
@@ -51,16 +52,18 @@ public:
     LLVMTypeConverter converter(&ctx);
 
     ConversionTarget target(ctx);
-    target.addLegalDialect<migraphx::MIGraphXDialect, StandardOpsDialect, gpu::GPUDialect, memref::MemRefDialect, LLVM::LLVMDialect>();
+    target.addLegalDialect<migraphx::MIGraphXDialect, StandardOpsDialect,
+                           gpu::GPUDialect, memref::MemRefDialect,
+                           LLVM::LLVMDialect>();
     target.addDynamicallyLegalOp<CallOp>([&](Operation *op) {
       auto fnAttr = op->getAttrOfType<FlatSymbolRefAttr>("callee");
-      auto fusedFuncOp = op->getParentOfType<ModuleOp>().lookupSymbol<FuncOp>(fnAttr.getValue());
-      return (fusedFuncOp.getOperation()->getAttr("kernel") == nullptr); 
+      auto fusedFuncOp = op->getParentOfType<ModuleOp>().lookupSymbol<FuncOp>(
+          fnAttr.getValue());
+      return (fusedFuncOp.getOperation()->getAttr("kernel") == nullptr);
     });
 
     FuncOp func = getFunction();
-    mlir::migraphx::populateFuncToCOBJPatterns(
-        func.getContext(), &patterns);
+    mlir::migraphx::populateFuncToCOBJPatterns(func.getContext(), &patterns);
 
     if (failed(applyPartialConversion(func, target, std::move(patterns)))) {
       signalPassFailure();
