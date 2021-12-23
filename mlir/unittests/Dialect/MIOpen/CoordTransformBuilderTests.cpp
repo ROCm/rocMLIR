1b63c88ca605eebe1e463d30433c50c85f807f0d
Krzysztof Drewniak <Krzysztof.Drewniak@amd.com>
2021-12-23 01:35:47 +0000

    Broken unit test support

---
+++ mlir/unittests/Dialect/MIOpen/CoordTransformBuilderTests.cpp
@@ -0,0 +1,17 @@
+//===- CoordTransformBuilderTests.cpp - Tests for the MIOpen Coordinate Transform Builder -----===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/Dialect/MIOpen/MIOpen.h"
+
+#include "gmock/gmock.h"
+
+using namespace mlir;
+
+//===----------------------------------------------------------------------===//
+// Test Fixture
+//===----------------------------------------------------------------------===//
