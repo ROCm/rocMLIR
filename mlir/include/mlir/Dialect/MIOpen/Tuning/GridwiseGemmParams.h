ROCm/rocMLIR

commit abccf5c0554b83706ed634007efef53707ff0f12

Fix invalid out of bound vector access. (#498)
* Change initial tuning parameter set.

Add asserts to reject invalid bufferA/B element vector types in
xdlops_gemm_v2 lowering.

* Reject invalid tuning parameters.

Add a negative test.

* Improve the negative test.

mlir/include/mlir/Dialect/MIOpen/Tuning/GridwiseGemmParams.h
@@ -799,9 +799,8 @@ private:
   // Initial tuning parameters for forward convolution.
   llvm::SmallVector<InitParamsXDL, 4> initParametersForward = {
       // M/block N/block K/block M/wave N/wave kPack aCopyMore bCopyMore
-      {128, 128, 2, 64, 64, 2, false, false},
-      {128, 128, 4, 64, 64, 2, false, false},
-      {32, 64, 2, 32, 64, 2, false, false},
+      {128, 128, 4, 64, 64, 4, false, false},
+      {32, 64, 4, 32, 64, 4, false, false},
 
       {128, 128, 8, 64, 64, 1, false, false},
       {128, 128, 16, 64, 64, 1, false, false},
@@ -931,11 +930,18 @@ private:
     if ((param.gemmKPerBlock % param.gemmKPack) != 0)
       return failure();
 
-    // Reject too wide KPACK values for fp32/fp16/bf16 types.
+    // Reject invalid KPACK values.
     auto dataType = ctx.getDataType();
+    // For fp32: reject anything wider than 4.
+    // For fp16/bf16: reject anything narrower than 4, or greater than 8.
     if (dataType.isF32() && param.gemmKPack > 4) {
+      llvm::errs() << "Invalid KPACK tuning parameter: " << param.gemmKPack
+                   << "\n";
       return failure();
-    } else if ((dataType.isF16() || dataType.isBF16()) && param.gemmKPack > 8) {
+    } else if ((dataType.isF16() || dataType.isBF16()) &&
+               ((param.gemmKPack < 4) || (param.gemmKPack > 8))) {
+      llvm::errs() << "Invalid KPACK tuning parameter: " << param.gemmKPack
+                   << "\n";
       return failure();
     }
 

mlir/lib/Dialect/MIOpen/Transforms/ThreadwiseToStdlib.cpp
@@ -2764,10 +2764,10 @@ struct XdlopsGemmV2RewritePattern : public OpRewritePattern<XdlopsGemmV2Op> {
       auto outerLoopb = OpBuilder::atBlockBegin(outerLoop.getBody());
       auto outerLoopiv = outerLoop.getInductionVar();
 
-      Type bufferAElementType =
-          op.bufferA().getType().template cast<MemRefType>().getElementType();
-      Type bufferBElementType =
-          op.bufferB().getType().template cast<MemRefType>().getElementType();
+      MemRefType bufferAType = op.bufferA().getType().cast<MemRefType>();
+      MemRefType bufferBType = op.bufferA().getType().cast<MemRefType>();
+      Type bufferAElementType = bufferAType.getElementType();
+      Type bufferBElementType = bufferBType.getElementType();
       Value bufferAElement = outerLoopb.create<memref::LoadOp>(
           loc, bufferAElementType, op.bufferA(), ValueRange{outerLoopiv});
       Value bufferBElement = outerLoopb.create<memref::LoadOp>(
@@ -2800,6 +2800,14 @@ struct XdlopsGemmV2RewritePattern : public OpRewritePattern<XdlopsGemmV2Op> {
           // argA/B to be supplied to MFMA XDLOPS are also vectors.
           assert(bufferAElementType.isa<VectorType>());
           assert(bufferBElementType.isa<VectorType>());
+          assert(bufferAElementType.cast<VectorType>().getShape().size() == 1);
+          assert(bufferBElementType.cast<VectorType>().getShape().size() == 1);
+          assert(bufferAElementType.cast<VectorType>().getShape()[0] %
+                     argTypeVectorLength ==
+                 0);
+          assert(bufferBElementType.cast<VectorType>().getShape()[0] %
+                     argTypeVectorLength ==
+                 0);
 
           argA = innerLoopb.create<SplatOp>(loc, zeroOp, argType);
           argB = innerLoopb.create<SplatOp>(loc, zeroOp, argType);
@@ -2833,6 +2841,8 @@ struct XdlopsGemmV2RewritePattern : public OpRewritePattern<XdlopsGemmV2Op> {
           // argA/B to be supplied to MFMA XDLOPS are scalars.
           assert(bufferAElementType.isa<VectorType>());
           assert(bufferBElementType.isa<VectorType>());
+          assert(bufferAElementType.cast<VectorType>().getShape().size() == 1);
+          assert(bufferBElementType.cast<VectorType>().getShape().size() == 1);
 
           argA = innerLoopb.create<vector::ExtractElementOp>(
               loc, dataType, bufferAElement, innerLoopiv);

mlir/test/Dialect/MIOpen/lowering_affix_params_negative.mlir
@@ -1,0 +1,10 @@
+// Negative test to deliberately pass incorrect tuning parameters.
+
+// RUN: miopen-gen -p -x2 -t f16 --perf_config "128,128,2,64,64,2,1,1" | miopen-opt -miopen-affix-params -verify-diagnostics
+// expected-error{{Incorrect KPACK tuning parameter: 2}}
+
+// RUN: miopen-gen -p -x2 -t f16 --perf_config "128,128,2,64,64,16,1,1" | miopen-opt -miopen-affix-params -verify-diagnostics
+// expected-error{{Incorrect KPACK tuning parameter: 16}}
+
+// RUN: miopen-gen -p -x2 -t f32 --perf_config "128,128,8,64,64,8,1,1" | miopen-opt -miopen-affix-params -verify-diagnostics
+// expected-error{{Incorrect KPACK tuning parameter: 8}}

commit 00be39c0ee69acad49c79ae9537211602e78450e

Fix side effects caused by #498. (#501)
* Allow KPACK be 1 for f16.

We are essentially rejecting KPACK=2 for f16.

* Reject tuning parameters when K can't be divided by KPerBlock * KPACK.

Move isValidGemm logic from PopulateParamsBase to its subclasses and
apply KPACK-specific computation.

mlir/include/mlir/Dialect/MIOpen/Tuning/GridwiseGemmParams.h
@@ -567,15 +567,6 @@ protected:
     return (gemmSize.gemmM / param->gemmMPerBlock) *
            (gemmSize.gemmN / param->gemmNPerBlock) * gemmSize.gemmG;
   }
-
-  mlir::LogicalResult isValidGemm(InitParams *param, GemmSize &gemmSize) {
-    if (!(gemmSize.gemmM % param->gemmMPerBlock == 0 &&
-          gemmSize.gemmN % param->gemmNPerBlock == 0 &&
-          gemmSize.gemmK % param->gemmKPerBlock == 0)) {
-      return mlir::failure();
-    }
-    return mlir::success();
-  }
 };
 
 struct InitParamsNonXDL : InitParams, Serializable<InitParamsNonXDL> {
@@ -792,6 +783,15 @@ public:
   }
 
   InitParams getUniversalParameters() { return universal_Parameters; }
+
+  LogicalResult isValidGemm(InitParamsNonXDL *param, GemmSize &gemmSize) {
+    if (!(gemmSize.gemmM % param->gemmMPerBlock == 0 &&
+          gemmSize.gemmN % param->gemmNPerBlock == 0 &&
+          gemmSize.gemmK % param->gemmKPerBlock == 0)) {
+      return mlir::failure();
+    }
+    return mlir::success();
+  }
 };
 
 class PopulateParamsXDL : public PopulateParamsBase {
@@ -939,6 +939,7 @@ private:
                    << "\n";
       return failure();
     } else if ((dataType.isF16() || dataType.isBF16()) &&
+               (param.gemmKPack != 1) &&
                ((param.gemmKPack < 4) || (param.gemmKPack > 8))) {
       llvm::errs() << "Invalid KPACK tuning parameter: " << param.gemmKPack
                    << "\n";
@@ -1019,6 +1020,15 @@ public:
   }
 
   InitParams getUniversalParameters() { return universal_Parameters; }
+
+  LogicalResult isValidGemm(InitParamsXDL *param, GemmSize &gemmSize) {
+    if (!(gemmSize.gemmM % param->gemmMPerBlock == 0 &&
+          gemmSize.gemmN % param->gemmNPerBlock == 0 &&
+          gemmSize.gemmK % (param->gemmKPerBlock * param->gemmKPack) == 0)) {
+      return mlir::failure();
+    }
+    return mlir::success();
+  }
 };
 
 #endif // MLIR_DIALECT_MIOPEN_GRIDWISE_GEMM_PARAMS_H