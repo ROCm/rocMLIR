adc3a28780926aa752e356bee564d82aa2aadfd6
Yi Qian <yi.qian@amd.com>
2022-05-24 02:19:04 +0000

    test

--- mlir/tools/miopen-gen/miopen-gen.cpp
+++ mlir/tools/miopen-gen/miopen-gen.cpp
@@ -1,4 +1,8 @@
 //===- miopen-gen.cpp - MLIR MIOpen Test Generator ------------------------===//
+
+
+
+
 //
 // Part of the MLIR Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
