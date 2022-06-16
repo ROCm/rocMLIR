a10c68e0f3a4842925ee7ce9addcf1e721504ad3
Paul Fuqua <pf@acm.org>
2022-06-16 14:24:06 -0500

    Update tests to account for new default anchor ops.

--- external/llvm-project/mlir/lib/Dialect/Tosa/Transforms/TosaPartition.cpp
+++ external/llvm-project/mlir/lib/Dialect/Tosa/Transforms/TosaPartition.cpp
@@ -425,7 +425,7 @@ public:
 class SimpleDefaultPartitionConfig : public PartitionConfig {
 public:
   bool isAnchorOp(Operation *op) override {
-    return isa<Conv2DOp,MatMulOp,DepthwiseConv2DOp>(op);
+    return isa<tosa::Conv2DOp,MatMulOp,DepthwiseConv2DOp>(op);
   }
   bool isLeadingOp(Operation *op) override {
     return isConstantZero(op) || isFuseableOp(op);
@@ -672,13 +672,14 @@ public:
       };
       pm.addPass(
           tosa::createTosaPartitionPass(new DepthwiseOnlyPartitionConfig()));
-    } else if (both) {
+    } else if (convOnly) {
       class DepthwiseAlsoPartitionConfig
           : public mlir::tosa::SimpleDefaultPartitionConfig {
       public:
         bool isAnchorOp(Operation *op) override {
-          return isa<tosa::Conv2DOp, tosa::DepthwiseConv2DOp>(op);
+          return isa<tosa::Conv2DOp>(op);
         }
+        std::string attributeName() override { return "four"; }
       };
       pm.addPass(
           tosa::createTosaPartitionPass(new DepthwiseAlsoPartitionConfig()));
@@ -686,6 +687,9 @@ public:
       class AttributeOnePartitionConfig
           : public mlir::tosa::SimpleDefaultPartitionConfig {
       public:
+        bool isAnchorOp(Operation *op) override {
+          return isa<tosa::Conv2DOp>(op);
+        }
         std::string attributeName() override { return "one"; }
       };
       pm.addPass(
@@ -705,8 +709,8 @@ public:
   Option<bool> defaultCase{*this, "default", llvm::cl::desc("Default.")};
   Option<bool> depthwiseOnly{*this, "depthwise-only",
                              llvm::cl::desc("Depthwise only.")};
-  Option<bool> both{*this, "both-conv-ops",
-                    llvm::cl::desc("Both depthwise-conv2d and conv2d.")};
+  Option<bool> convOnly{*this, "conv-only",
+                    llvm::cl::desc("Only conv2d.")};
   Option<bool> attrOne{*this, "attr-one",
                        llvm::cl::desc("Attribute-name 'one'.")};
   Option<bool> nofrontArg{*this, "nofront-arg",
