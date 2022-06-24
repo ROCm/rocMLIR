f34bbc8700d205868ee2a8e421079a0b215eaad1
jungpark-mlir <jungwook.park@amd.com>
2022-06-24 14:24:49 +0100

    Update MIGraphX.cpp

--- mlir/lib/CAPI/Dialect/MIGraphX.cpp
+++ mlir/lib/CAPI/Dialect/MIGraphX.cpp
@@ -103,6 +103,8 @@ MLIR_CAPI_EXPORTED bool mlirGetBinary(MlirModule module, int *size, char *bin) {
 MLIR_CAPI_EXPORTED
 void mlirMIGraphXAddHighLevelPipeline(MlirPassManager pm) {
   auto passMan = unwrap(pm);
+  // FIXME : WA for the multithreading issue, potentially fixed in upstream.
+  passMan->getContext()->disableMultithreading();
   passMan->setNesting(mlir::PassManager::Nesting::Implicit);
   mlir::migraphx::addHighLevelPipeline(*passMan);
   mlir::miopen::buildBufferizePipeline(*passMan);
