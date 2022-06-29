46c93e8018b3491e1b7d3741901055a61ef55b48
Krzysztof Drewniak <Krzysztof.Drewniak@amd.com>
2022-06-28 20:46:17 -0500

    Define a shell script for building our Dockerfile (#674)
    * Define a shell script for building our Dockerfile
    
    Currently, the Dockerfile build is inline in a Jenkins job config,
    which makes it impossible to track changes and. Move the logic from
    that build pipeline (with minor refactoring) to a script that the
    Jenkins job can call.
    
    This PR will have no effect until the rocm-docker job on ml-ci.amd.com
    is changed to use the new script.
    
    * Amend `rocm_full_version` to parse Dockerfile.rocm
    
    * Add a "force" option
    
    Since the Jenkins job isn't set up as a pipeline, we can't add
    parameters, but we can make the script take an argument to make all
    this easier.
    
    Co-authored-by: Zhuoran Yin <zhuoryin@amd.com>

---
+++ mlir/utils/jenkins/build-dockerfile.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -x
+
+declare -a ARGS=("$@")
+
+err() {
+  echo "$*" >&2
+}
+
+# Clean up the docker images
+# Note: includes a workaround for the occasional hung docker daemon
+start_clean_docker() {
+  systemctl status docker | grep 'Active:'
+  sudo /usr/bin/pkill -f docker
+  sudo /bin/systemctl restart docker
+  docker system prune -a -f
+  systemctl status docker | grep 'Active:'
+}
+
+# Build the docker image and push it to the repository
+build_rocm_image() {
+  if docker build -t rocm-mlir -f ./mlir/utils/jenkins/Dockerfile.rocm ./mlir/utils/jenkins ; then
+    err Docker image build failed
+    exit 1
+  fi
+
+  # Target repository to push
+  local docker_repository="rocm/mlir"
+
+  local rocm_full_version rocm_short_version git_commit_hash
+  rocm_full_version=$(grep "ROCM_PATH" ./mlir/utils/jenkins/Dockerfile.rocm | sed 's/.*-\([0-9][0-9]*[.][0-9][0-9.]*\)/\1/')
+  rocm_short_version="rocm${rocm_full_version%.*}"
+  git_commit_hash=$(git rev-parse --short HEAD)
+
+  # Example: rocm/mlir:rocm3.7-38337614c80
+  local docker_new_img_name=${docker_repository}:${rocm_short_version}-${git_commit_hash}
+  # Example: rocm/mlir:rocm3.7-latest
+  local docker_new_img_name_rocm_latest=${docker_repository}:${rocm_short_version}-latest
+  # Example: rocm/mlir:latest
+  local docker_new_img_name_latest=${docker_repository}:latest
+
+  # Commit the most recent container to an image with the new image name
+  docker tag rocm-mlir "${docker_new_img_name_rocm_latest}"
+  docker tag rocm-mlir "${docker_new_img_name_latest}"
+  docker tag rocm-mlir "${docker_new_img_name}"
+
+  if docker login -u="${USER}" -p="${PASSWORD}" ; then
+    err Docker login failed
+    exit 1
+  fi
+
+  docker push "${docker_new_img_name}"
+  docker push "${docker_new_img_name_rocm_latest}"
+  docker push "${docker_new_img_name_latest}"
+  docker logout
+}
+
+main() {
+  if [[ " ${ARGS[*]} " =~ " --force " ]] || \
+      git diff --name-only HEAD^ HEAD | grep -q "Dockerfile.rocm"; then
+    echo "Start a new build"
+    start_clean_docker
+    build_rocm_image
+  else
+    echo "No dependency changes, abort build"
+    exit 0
+  fi
+}
+
+main
