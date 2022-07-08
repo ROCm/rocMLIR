f7d85546ad9007efbbb1626148c9109ee2c8a752
Lixun Zhang <lixun.zhang@amd.com>
2022-07-08 18:48:00 +0000

    Used configure_package_config_file() to auto generate the config file

--- mlir/tools/mlir-miopen-lib/cmake/MLIRMIOpenConfig.cmake
+++
@@ -1,4 +0,0 @@
-# Auto compute the include dir for the header file
-get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-include("${SELF_DIR}/MLIRMIOpenTargets.cmake")
-get_filename_component(MLIRMIOpen_INCLUDE_DIR "${SELF_DIR}/../../../include/@PACKAGE_DIR@" ABSOLUTE)
