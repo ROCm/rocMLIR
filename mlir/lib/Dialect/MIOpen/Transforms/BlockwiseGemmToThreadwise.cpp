969cbf3424441549d022ffa0f1fd1eea8e3baa2d
jerryyin <zhuoryin@amd.com>
2022-07-18 20:55:19 +0000

    Moving lds to register load into blockwisegemm

--- mlir/lib/Dialect/MIOpen/Transforms/BlockwiseGemmToThreadwise.cpp
+++ mlir/lib/Dialect/MIOpen/Transforms/BlockwiseGemmToThreadwise.cpp
@@ -29,6 +29,7 @@
 #include "mlir/Dialect/MIOpen/utility/loweringUtils.h"
 
 #include "mlir/Dialect/Affine/IR/AffineOps.h"
+#include "mlir/Dialect/MIOpen/XdlopsCodeSelection.h"
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Transforms/DialectConversion.h"
 
@@ -240,10 +241,17 @@ struct BlockwiseGemmV2RewritePattern
                                 ConversionPatternRewriter &b) const override {
     Location loc = op.getLoc();
 
+    int64_t M = op->getAttr("m").template cast<IntegerAttr>().getInt();
+    int64_t N = op->getAttr("n").template cast<IntegerAttr>().getInt();
+    int64_t K = op->getAttr("k").template cast<IntegerAttr>().getInt();
     int64_t MPerWave =
         op->getAttr("m_per_wave").template cast<IntegerAttr>().getInt();
     int64_t NPerWave =
         op->getAttr("n_per_wave").template cast<IntegerAttr>().getInt();
+    int64_t KPack =
+        op->hasAttr("kpack")
+            ? op->getAttr("kpack").template cast<IntegerAttr>().getInt()
+            : 1;
 
     // Original C++ logic.
     // static constexpr index_t MRepeats = (GemmMPerWave > 64) ? (GemmMPerWave /
@@ -257,6 +265,233 @@ struct BlockwiseGemmV2RewritePattern
     int64_t MPerXdlops = (MPerWave > 64) ? 64 : MPerWave;
     int64_t NPerXdlops = (NPerWave > 64) ? 64 : NPerWave;
 
+    int64_t ldsOffsetA = op.ldsBufferOffsetA().getSExtValue();
+    int64_t ldsOffsetB = op.ldsBufferOffsetB().getSExtValue();
+
+    assert(ldsOffsetA % KPack == 0 &&
+           "LDS buffer segment for A is kpack-aligned");
+    assert(ldsOffsetB % KPack == 0 &&
+           "LDS buffer segment for B is kpack-aligned");
+    auto dataType = adaptor.matrixA()
+                        .getType()
+                        .template cast<MemRefType>()
+                        .getElementType();
+
+    // The address calculations into the LDS buffer assume that the buffer
+    // has type vector<KPack x T>. Then, we convert that into an address
+    // in a buffer of Ts through a final multiplicaiton by KPack.
+    // However, the LDS buffer offset, which was computed when the buffer was
+    // allocated, is an offset into a buffer of T. Therefore, to allow it to
+    // easily participate in adress calculations (instead of adding it on at the
+    // end) we must divide it by KPack here. Fortunately, this offset will be
+    // KPack-alligned and so this is safe
+    Value aBase =
+        b.create<AddIOp>(loc, adaptor.waveOffsetA(),
+                         b.create<ConstantIndexOp>(loc, ldsOffsetA / KPack));
+    Value bBase =
+        b.create<AddIOp>(loc, adaptor.waveOffsetB(),
+                         b.create<ConstantIndexOp>(loc, ldsOffsetB / KPack));
+
+    XdlopsCodeSelection xcs =
+        XdlopsCodeSelection::get(dataType, MPerWave, NPerWave, b);
+
+    // Extract values from XdlopsCodeSelection.
+    amdgpu::MFMAInstr mfmaInstr = xcs.instr;
+    LLVM_DEBUG(llvm::dbgs() << "Selected xdlop: "
+                            << amdgpu::stringifyMFMAInstr(mfmaInstr) << "\n");
+    SmallVector<SmallVector<unsigned, 3>, 2> imms = xcs.imms;
+    Type argType = xcs.argType;
+
+    int64_t num_threads_blk = xcs.num_threads_blk;
+    int64_t num_input_blks = xcs.num_input_blks;
+    int64_t num_output_blks = xcs.num_output_blks;
+    int64_t k_base = xcs.k_base;
+
+    bool IsKReduction = (num_output_blks == 1) && (num_input_blks > 1);
+
+    if (KPack > 1 && (KPack < k_base || KPack % k_base != 0)) {
+      llvm_unreachable(
+          "Tuning parameter selection guarantees kPack is multiple of k_base,"
+          "this should never happen");
+    }
+
+    // const index_t laneId = get_thread_local_1d_id() % mfma_type.wave_size;
+    // FloatA a[KPerThread * MRepeats];
+    // FloatB b[KPerThread * NRepeats];
+    // constexpr index_t KRepeats = KPack / mfma_type.k_base;
+    // auto pa = reinterpret_cast<const data_type*>(&a);
+    // auto pb = reinterpret_cast<const data_type*>(&b);
+    // constexpr index_t AStride = KPerThread * KRepeats;
+    // constexpr index_t BStride = KPerThread * KRepeats;
+
+    auto tid = b.create<WorkitemIdOp>(loc, b.getIndexType());
+    constexpr int64_t waveSize = 64;
+    auto laneId =
+        b.create<RemUIOp>(loc, tid, b.create<ConstantIndexOp>(loc, waveSize));
+
+    LLVM_DEBUG(llvm::dbgs()
+               << "argVectorType: " << argType << "\n"
+               << "k_base: " << k_base << "\n"
+               << "K: " << K << "\n"
+               << "bufferA type: " << adaptor.bufferA().getType() << "\n"
+               << "bufferB type: " << adaptor.bufferB().getType() << "\n");
+
+    auto MConstantOp = b.create<ConstantIndexOp>(loc, M);
+    auto NConstantOp = b.create<ConstantIndexOp>(loc, N);
+    auto KConstantOp = b.create<ConstantIndexOp>(loc, K);
+
+    auto MPerXdlopsConstantOp = b.create<ConstantIndexOp>(loc, MPerXdlops);
+    auto NPerXdlopsConstantOp = b.create<ConstantIndexOp>(loc, NPerXdlops);
+
+    Value bufferA = adaptor.bufferA();
+    Value bufferB = adaptor.bufferB();
+    auto bufferAType = adaptor.bufferA().getType().cast<MemRefType>();
+    auto bufferBType = adaptor.bufferB().getType().cast<MemRefType>();
+    Type bufferAElementType = bufferAType.getElementType();
+    Type bufferBElementType = bufferBType.getElementType();
+
+    int64_t KPerThread = IsKReduction ? K / num_input_blks : K;
+    Value zeroConstantOp = b.createOrFold<ConstantIndexOp>(loc, 0);
+    auto KPerBlockConstantOp = b.create<ConstantIndexOp>(loc, KPerThread);
+
+    if (!IsKReduction) {
+
+      // store bufferA logic.
+      // for(index_t m_i = 0; m_i < MRepeats; ++m_i)
+      //   for(index_t k_i      = 0; k_i < K; ++k_i)
+      //     a[k_i + m_i * K] = p_a_wave[k_i * M + laneId + MPerXdlops * m_i];
+      // Note: p_a_wave need to be offseted by waveOffsetA.
+
+      auto outerLoopM = b.create<AffineForOp>(loc, 0, MRepeats);
+      auto olmb = ConversionPatternRewriter::atBlockBegin(outerLoopM.getBody(),
+                                                          b.getListener());
+      auto olmiv = outerLoopM.getInductionVar();
+      auto mOffset = olmb.create<AddIOp>(
+          loc, aBase, olmb.create<MulIOp>(loc, MPerXdlopsConstantOp, olmiv));
+      auto kOffsetA = olmb.create<MulIOp>(loc, olmiv, KConstantOp);
+
+      auto innerLoopMK = olmb.create<AffineForOp>(loc, 0, KPerThread);
+      auto ilmkb = ConversionPatternRewriter::atBlockBegin(
+          innerLoopMK.getBody(), olmb.getListener());
+      auto ilmkiv = innerLoopMK.getInductionVar();
+
+      Value sourceOffsetA = ilmkb.create<AddIOp>(
+          loc,
+          ilmkb.create<AddIOp>(
+              loc, ilmkb.create<MulIOp>(loc, ilmkiv, MConstantOp), laneId),
+          mOffset);
+
+      if (KPack > 1)
+        sourceOffsetA = ilmkb.create<MulIOp>(
+            loc, sourceOffsetA, ilmkb.create<ConstantIndexOp>(loc, KPack));
+
+      auto destOffsetA = ilmkb.create<AddIOp>(loc, ilmkiv, kOffsetA);
+
+      Value valueA = ilmkb.create<InBoundsLoadOp>(loc, bufferAElementType,
+                                                  op.matrixA(), sourceOffsetA);
+      ilmkb.create<memref::StoreOp>(loc, valueA, bufferA,
+                                    ValueRange{destOffsetA});
+
+      // store bufferB logic.
+      // for(index_t n_i = 0; n_i < NRepeats; ++n_i)
+      //   for(index_t k_i      = 0; k_i < KPerThread; ++k_i)
+      //     b[k_i + n_i * KPerThread] = p_b_wave[k_i * N + laneId + NPerXdlops
+      //     * n_i];
+      // Note: p_b_wave need to be offseted by waveOffsetB.
+
+      auto outerLoopN = b.create<AffineForOp>(loc, 0, NRepeats);
+      auto olnb = ConversionPatternRewriter::atBlockBegin(outerLoopN.getBody(),
+                                                          b.getListener());
+      auto olniv = outerLoopN.getInductionVar();
+      auto nOffset = olnb.create<AddIOp>(
+          loc, bBase, olnb.create<MulIOp>(loc, NPerXdlopsConstantOp, olniv));
+      auto kOffsetB = olnb.create<MulIOp>(loc, olniv, KConstantOp);
+
+      auto innerLoopNK = olnb.create<AffineForOp>(loc, 0, KPerThread);
+      auto ilnkb = ConversionPatternRewriter::atBlockBegin(
+          innerLoopNK.getBody(), olnb.getListener());
+      auto ilnkiv = innerLoopNK.getInductionVar();
+
+      Value sourceOffsetB = ilnkb.create<AddIOp>(
+          loc,
+          ilnkb.create<AddIOp>(
+              loc, ilnkb.create<MulIOp>(loc, ilnkiv, NConstantOp), laneId),
+          nOffset);
+
+      if (KPack > 1)
+        sourceOffsetB = ilnkb.create<MulIOp>(
+            loc, sourceOffsetB, ilnkb.create<ConstantIndexOp>(loc, KPack));
+
+      auto destOffsetB = ilnkb.create<AddIOp>(loc, ilnkiv, kOffsetB);
+
+      Value valueB = ilnkb.create<InBoundsLoadOp>(loc, bufferBElementType,
+                                                  op.matrixB(), sourceOffsetB);
+      ilnkb.create<memref::StoreOp>(loc, valueB, bufferB,
+                                    ValueRange{destOffsetB});
+    } else {
+      // const index_t blk_id = laneId / mfma_type.num_threads_blk;
+      // const index_t blk_td = laneId % mfma_type.num_threads_blk;
+      auto NumThreadsBlkConstantOp =
+          b.create<ConstantIndexOp>(loc, num_threads_blk);
+      auto blk_id = b.create<DivUIOp>(loc, laneId, NumThreadsBlkConstantOp);
+      auto blk_td = b.create<RemUIOp>(loc, laneId, NumThreadsBlkConstantOp);
+
+      Value kBaseA = b.create<AddIOp>(loc, aBase, blk_td);
+      Value kBaseB = b.create<AddIOp>(loc, bBase, blk_td);
+
+      // for(index_t k_i = 0; k_i < KPerThread; k_i += mfma_type.num_input_blks)
+      // {
+      //     a[k_i] = p_a_wave[(k_i * num_input_blks + blk_id) * M + blk_td];
+      //     b[k_i] = p_b_wave[(k_i * num_input_blks + blk_id) * N + blk_td];
+      // }
+      // p_a_wave need to be offseted by waveOffsetA.
+      // p_b_wave need to be offseted by waveOffsetB.
+
+      auto NumInputBlksConstantOp =
+          b.create<ConstantIndexOp>(loc, num_input_blks);
+
+      auto loopKLoad = b.create<AffineForOp>(loc, 0, KPerThread);
+      auto lklb = ConversionPatternRewriter::atBlockBegin(loopKLoad.getBody(),
+                                                          b.getListener());
+      auto lkliv = loopKLoad.getInductionVar();
+
+      Value sourceOffsetA = lklb.create<AddIOp>(
+          loc,
+          lklb.create<MulIOp>(
+              loc,
+              lklb.create<AddIOp>(
+                  loc, lklb.create<MulIOp>(loc, lkliv, NumInputBlksConstantOp),
+                  blk_id),
+              MConstantOp),
+          kBaseA);
+
+      if (KPack > 1)
+        sourceOffsetA = lklb.create<MulIOp>(
+            loc, sourceOffsetA, lklb.create<ConstantIndexOp>(loc, KPack));
+
+      Value valueA = lklb.create<InBoundsLoadOp>(loc, bufferAElementType,
+                                                 op.matrixA(), sourceOffsetA);
+      lklb.create<memref::StoreOp>(loc, valueA, bufferA, ValueRange{lkliv});
+
+      Value sourceOffsetB = lklb.create<AddIOp>(
+          loc,
+          lklb.create<MulIOp>(
+              loc,
+              lklb.create<AddIOp>(
+                  loc, lklb.create<MulIOp>(loc, lkliv, NumInputBlksConstantOp),
+                  blk_id),
+              NConstantOp),
+          kBaseB);
+
+      if (KPack > 1)
+        sourceOffsetB = lklb.create<MulIOp>(
+            loc, sourceOffsetB, lklb.create<ConstantIndexOp>(loc, KPack));
+
+      Value valueB = lklb.create<InBoundsLoadOp>(loc, bufferBElementType,
+                                                 op.matrixB(), sourceOffsetB);
+      lklb.create<memref::StoreOp>(loc, valueB, bufferB, ValueRange{lkliv});
+    }
+
     if (MRepeats == 1 && NRepeats == 1) {
       SmallVector<Type, 2> resultTypes;
       for (auto result : op.vectorDs()) {
@@ -265,8 +500,8 @@ struct BlockwiseGemmV2RewritePattern
 
       auto xdlopsGemmV2Op = b.replaceOpWithNewOp<XdlopsGemmV2Op>(
           op, resultTypes, adaptor.matrixA(), adaptor.matrixB(),
-          op.ldsBufferOffsetA(), op.ldsBufferOffsetB(), adaptor.waveOffsetA(),
-          adaptor.waveOffsetB(), adaptor.bufferA(), adaptor.bufferB(),
+          op.ldsBufferOffsetA(), op.ldsBufferOffsetB(), zeroConstantOp,
+          zeroConstantOp, adaptor.bufferA(), adaptor.bufferB(),
           adaptor.vectorCs());
 
       xdlopsGemmV2Op->setAttr("m", op->getAttr("m"));
@@ -278,9 +513,10 @@ struct BlockwiseGemmV2RewritePattern
         xdlopsGemmV2Op->setAttr("kpack", op->getAttr("kpack"));
     } else if (MRepeats == 2 && NRepeats == 1) {
       // Original C++ logic.
-      // p_c_thread.s.x.l = XdlopsGemm.template Run<M, N, K>(p_a_block,
-      // p_b_block, p_c_thread.s.x.l); p_c_thread.s.y.l = XdlopsGemm.template
-      // Run<M, N, K>(p_a_block + MPerXdlops, p_b_block, p_c_thread.s.y.l);
+      // p_c_thread.s.x.l = XdlopsGemm.template Run<M, N, K>(
+      // p_a_block, p_b_block, p_c_thread.s.x.l);
+      // p_c_thread.s.y.l = XdlopsGemm.templateRun<M, N, K>(
+      // p_a_block + MPerXdlops, p_b_block, p_c_thread.s.y.l);
 
       SmallVector<Type, 2> resultTypes0;
       resultTypes0.push_back(op.vectorDs()[0].getType());
@@ -288,8 +524,8 @@ struct BlockwiseGemmV2RewritePattern
 
       auto xdlopsGemmV2Op0 = b.create<XdlopsGemmV2Op>(
           loc, resultTypes0, adaptor.matrixA(), adaptor.matrixB(),
-          op.ldsBufferOffsetA(), op.ldsBufferOffsetB(), adaptor.waveOffsetA(),
-          adaptor.waveOffsetB(), adaptor.bufferA(), adaptor.bufferB(),
+          op.ldsBufferOffsetA(), op.ldsBufferOffsetB(), zeroConstantOp,
+          zeroConstantOp, adaptor.bufferA(), adaptor.bufferB(),
           adaptor.vectorCs().take_front(2));
 
       xdlopsGemmV2Op0->setAttr("m", op->getAttr("m"));
@@ -306,12 +542,10 @@ struct BlockwiseGemmV2RewritePattern
       resultTypes1.push_back(op.vectorDs()[2].getType());
       resultTypes1.push_back(op.vectorDs()[3].getType());
 
-      auto MPerXdlopsConstantOp = b.create<ConstantIndexOp>(loc, MPerXdlops);
       auto xdlopsGemmV2Op1 = b.create<XdlopsGemmV2Op>(
           loc, resultTypes1, adaptor.matrixA(), adaptor.matrixB(),
-          op.ldsBufferOffsetA(), op.ldsBufferOffsetB(),
-          b.create<AddIOp>(loc, adaptor.waveOffsetA(), MPerXdlopsConstantOp),
-          adaptor.waveOffsetB(), adaptor.bufferA(), adaptor.bufferB(),
+          op.ldsBufferOffsetA(), op.ldsBufferOffsetB(), KPerBlockConstantOp,
+          zeroConstantOp, adaptor.bufferA(), adaptor.bufferB(),
           adaptor.vectorCs().drop_front(2));
 
       xdlopsGemmV2Op1->setAttr("m", op->getAttr("m"));
@@ -330,9 +564,10 @@ struct BlockwiseGemmV2RewritePattern
                                  xdlopsGemmV2Op1.vectorDs()[1]});
     } else if (MRepeats == 1 && NRepeats == 2) {
       // Original C++ logic.
-      // p_c_thread.s.x.l = XdlopsGemm.template Run<M, N, K>(p_a_block,
-      // p_b_block, p_c_thread.s.x.l); p_c_thread.s.y.l = XdlopsGemm.template
-      // Run<M, N, K>(p_a_block, p_b_block + NPerXdlops, p_c_thread.s.y.l);
+      // p_c_thread.s.x.l = XdlopsGemm.template Run<M, N, K>(
+      // p_a_block, p_b_block, p_c_thread.s.x.l);
+      // p_c_thread.s.y.l = XdlopsGemm.template Run<M, N, K>(
+      // p_a_block, p_b_block + NPerXdlops, p_c_thread.s.y.l);
 
       SmallVector<Type, 2> resultTypes0;
       resultTypes0.push_back(op.vectorDs()[0].getType());
@@ -340,8 +575,8 @@ struct BlockwiseGemmV2RewritePattern
 
       auto xdlopsGemmV2Op0 = b.create<XdlopsGemmV2Op>(
           loc, resultTypes0, adaptor.matrixA(), adaptor.matrixB(),
-          op.ldsBufferOffsetA(), op.ldsBufferOffsetB(), adaptor.waveOffsetA(),
-          adaptor.waveOffsetB(), adaptor.bufferA(), adaptor.bufferB(),
+          op.ldsBufferOffsetA(), op.ldsBufferOffsetB(), zeroConstantOp,
+          zeroConstantOp, adaptor.bufferA(), adaptor.bufferB(),
           adaptor.vectorCs().take_front(2));
 
       xdlopsGemmV2Op0->setAttr("m", op->getAttr("m"));
@@ -358,12 +593,10 @@ struct BlockwiseGemmV2RewritePattern
       resultTypes1.push_back(op.vectorDs()[2].getType());
       resultTypes1.push_back(op.vectorDs()[3].getType());
 
-      auto NPerXdlopsConstantOp = b.create<ConstantIndexOp>(loc, NPerXdlops);
       auto xdlopsGemmV2Op1 = b.create<XdlopsGemmV2Op>(
           loc, resultTypes1, adaptor.matrixA(), adaptor.matrixB(),
-          op.ldsBufferOffsetA(), op.ldsBufferOffsetB(), adaptor.waveOffsetA(),
-          b.create<AddIOp>(loc, adaptor.waveOffsetB(), NPerXdlopsConstantOp),
-          adaptor.bufferA(), adaptor.bufferB(),
+          op.ldsBufferOffsetA(), op.ldsBufferOffsetB(), zeroConstantOp,
+          KPerBlockConstantOp, adaptor.bufferA(), adaptor.bufferB(),
           adaptor.vectorCs().drop_front(2));
 
       xdlopsGemmV2Op1->setAttr("m", op->getAttr("m"));
