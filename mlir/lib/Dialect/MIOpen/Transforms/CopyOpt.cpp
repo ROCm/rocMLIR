ROCm/rocMLIR

commit 3002d65e90f4a4f5ee50323dc5f55b1de71a6cbf

Fusion fix for broadcast (#748)
* handle inputs with bcast

* CopyOpt to fold reshaped output

* Move up reshape

* Re-enable fusion tests

* drop reshape if possible

* fix from review

mlir/lib/Dialect/MIOpen/Transforms/CopyOpt.cpp
@@ -59,6 +59,7 @@ template <typename T> struct MICORewritePattern : public OpRewritePattern<T> {
     // 1. Capture allocation->copy pattern
     Operation *writer = nullptr;
     memref::CopyOp reader = nullptr;
+    memref::CollapseShapeOp csop = nullptr;
     for (auto &use : mem.getUses()) {
       if (auto laop = dyn_cast<linalg::GenericOp>(use.getOwner())) {
         // 1.0 Output of linalg.generic
@@ -99,6 +100,19 @@ template <typename T> struct MICORewritePattern : public OpRewritePattern<T> {
         if (mrop.getSource() != mem)
           return fail;
         reader = mrop;
+      } else if (csop =
+                     dyn_cast<memref::CollapseShapeOp>(use.getOwner())) {
+        // 1.4 catch the case it has collapse shape befor the copy
+        if (reader)
+          return fail;
+        if (!use.getOwner()->hasOneUse())
+          return fail;
+        for (auto &cop : use.getOwner()->getUses()) {
+          if (auto mrop = dyn_cast<memref::CopyOp>(cop.getOwner())) {
+            reader = mrop;
+          } else
+            return fail;
+        }
       } else {
         // unsupported op
         return fail;
@@ -111,9 +125,14 @@ template <typename T> struct MICORewritePattern : public OpRewritePattern<T> {
 
       if (mem.getType() == realMem.getType()) {
         mem.replaceAllUsesWith(realMem);
-
         reader->erase();
-
+        return success();
+      } else if (csop) {
+        Location loc = mem.getLoc();
+        auto new_expand = b.create<memref::ExpandShapeOp>(
+          loc, mem.getType(), realMem, csop.getReassociation());
+        mem.replaceAllUsesWith(new_expand);
+        reader->erase();
         return success();
       }
     }

mlir/lib/Dialect/MIOpen/Transforms/AlignTiling.cpp
@@ -169,9 +169,16 @@ static void insertCopyFromOtherArg(PatternRewriter &b, Location loc,
                                    Value dest) {
   LLVM_DEBUG(llvm::dbgs() << "Src type: " << srcOp.getType()
                           << " dest type: " << op.dest().getType() << "\n");
-  assert(srcOp.getType().cast<ShapedType>().getShape() ==
-             op.dest().getType().cast<ShapedType>().getShape() &&
-         "shape of extra fusion arguments matches shape of C tensor");
+  ArrayRef<int64_t> sType, dType;
+  sType = srcOp.getType().cast<ShapedType>().getShape();
+  dType = op.dest().getType().cast<ShapedType>().getShape();
+  assert(sType.size() == dType.size() &&
+         "Rank of extra fusion arguments matches shape of C tensor");
+  for (unsigned i = 0; i < sType.size(); i++) {
+    assert((sType[i] == dType[i] || sType[i] == 1) &&
+           "shape of extra fusion arguments matches shape of C tensor or "
+           "broadcastable");
+  }
 
   auto writeCLoop = op->getParentOfType<TransformingForOp>();
   assert(writeCLoop && "threadwise_copy_v2 must be in a transforming_for");
@@ -288,6 +295,22 @@ static ThreadwiseCopyV2Op traceToThreadwiseCopy(Value inp) {
       allValidUses = false;
     }
   }
+
+  // Additionally catch the case when gemm result had to be expanded before
+  // being fed.
+  if (auto expanded = inp.getDefiningOp<memref::ExpandShapeOp>()) {
+    auto src = expanded.getSrc();
+    for (Operation *use : src.getUsers()) {
+      if (auto copy = dyn_cast<ThreadwiseCopyV2Op>(use)) {
+        if (result) {
+          LLVM_DEBUG(llvm::dbgs() << "Multiple copies somehow, no fusion\n");
+          allValidUses = false;
+        }
+        result = copy;
+      }
+    }
+  }
+
   if (!result)
     LLVM_DEBUG(llvm::dbgs() << "Align tiling: generic not tracing to copy");
   if (!allValidUses)
@@ -312,7 +335,8 @@ static Value reconfigureLAGeneric(PatternRewriter &b,
     if (Value inp = std::get<0>(pair)) {
       AffineMap inpIdxMap = std::get<1>(pair);
       Value newInput;
-      if (inp == twout) {
+      auto expanded = inp.getDefiningOp<memref::ExpandShapeOp>();
+      if (inp == twout || (expanded && expanded.getSrc() == twout)) {
         newInput = laIn;
       } else {
         // 2.1.1. Align tiling of other inputs
@@ -419,6 +443,12 @@ LogicalResult MILARewritePattern::matchAndRewrite(linalg::GenericOp laGeneric,
     return failure();
   }
   // 2. Apply if input found
+
+  // point back to original memory.
+  if (memref::ExpandShapeOp expanded =
+          out.getDefiningOp<memref::ExpandShapeOp>()) {
+    out = expanded.getSrc();
+  }
 
   Value gemmV2Outs = copyOp.source();
   auto gemmV2OutsType = gemmV2Outs.getType().cast<MemRefType>();

mlir/test/fusion/e2e/tosa-to-miopen-bcast-add.e2e.mlir
@@ -1,14 +1,12 @@
 // RUN: mlir-miopen-driver -host-pipeline highlevel %s | miopen-gen -ph -print-inputs -print-results -rand none - | mlir-miopen-driver -c  | mlir-rocm-runner --shared-libs=%linalg_test_lib_dir/libmlir_rocm_runtime%shlibext,%conv_validation_wrapper_library_dir/libconv-validation-wrappers%shlibext,%linalg_test_lib_dir/libmlir_runner_utils%shlibext -entry-point-result=void | FileCheck %s
 
 // CHECK: Unranked Memref base
-//func.func @test_fusion(%arg0: tensor<1x8x8x4xf32>, %arg1: tensor<8x1x1x4xf32>, %arg3: tensor<1x1x1x8xf32>) -> tensor<1x8x8x8xf32> attributes {kernel} {
-func.func @test_fusion(%arg0: tensor<1x8x8x4xf32>, %arg1: tensor<8x1x1x4xf32>) -> tensor<1x8x8x8xf32> attributes {kernel} {
+func.func @test_fusion(%arg0: tensor<1x8x8x4xf32>, %arg1: tensor<8x1x1x4xf32>, %arg3: tensor<1x1x1x8xf32>) -> tensor<1x8x8x8xf32> attributes {kernel} {
   %zero = arith.constant dense<0.0> : tensor<8xf32>
   %0 = "tosa.conv2d"(%arg0, %arg1, %zero) {dilation = [1, 1], pad = [0, 0, 0, 0], stride = [1, 1]} : (tensor<1x8x8x4xf32>, tensor<8x1x1x4xf32>, tensor<8xf32>) -> tensor<1x8x8x8xf32>
-//  %2 = "tosa.add"(%0, %arg3) {} : (tensor<1x8x8x8xf32>, tensor<1x1x1x8xf32>) -> tensor<1x8x8x8xf32>
+  %2 = "tosa.add"(%0, %arg3) {} : (tensor<1x8x8x8xf32>, tensor<1x1x1x8xf32>) -> tensor<1x8x8x8xf32>
 
-  //return %2 : tensor<1x8x8x8xf32>
-  return %0 : tensor<1x8x8x8xf32>
+  return %2 : tensor<1x8x8x8xf32>
 }
 
 // -----

mlir/test/fusion/e2e/tosa-to-miopen-bias.e2e.mlir
@@ -1,7 +1,4 @@
-// FIXME: mlir-miopen-driver -host-pipeline highlevel %s | miopen-gen -ph -print-results -rand none - | mlir-miopen-driver -c  | mlir-rocm-runner --shared-libs=%linalg_test_lib_dir/libmlir_rocm_runtime%shlibext,%conv_validation_wrapper_library_dir/libconv-validation-wrappers%shlibext,%linalg_test_lib_dir/libmlir_runner_utils%shlibext -entry-point-result=void | FileCheck %s
-
-// RUN: mlir-miopen-driver -h
-
+// RUN: mlir-miopen-driver -host-pipeline highlevel %s | miopen-gen -ph -print-results -rand none - | mlir-miopen-driver -c  | mlir-rocm-runner --shared-libs=%linalg_test_lib_dir/libmlir_rocm_runtime%shlibext,%conv_validation_wrapper_library_dir/libconv-validation-wrappers%shlibext,%linalg_test_lib_dir/libmlir_runner_utils%shlibext -entry-point-result=void | FileCheck %s
 // CHECK: Unranked Memref base
 func.func @test_fusion(%arg0: tensor<1x32x32x8xf32>, %arg1: tensor<16x3x3x8xf32>, %arg2: tensor<16xf32>) -> tensor<1x30x30x16xf32> attributes {kernel} {
   %0 = "tosa.conv2d"(%arg0, %arg1, %arg2) {dilation = [1, 1], pad = [0, 0, 0, 0], stride = [1, 1]} : (tensor<1x32x32x8xf32>, tensor<16x3x3x8xf32>, tensor<16xf32>) -> tensor<1x30x30x16xf32>

commit 53a5eb887ffe476e9df05fa842f4f44fff0c05e0

Fixing clang-format breakage from #748

mlir/lib/Dialect/MIOpen/Transforms/CopyOpt.cpp
@@ -100,8 +100,7 @@ template <typename T> struct MICORewritePattern : public OpRewritePattern<T> {
         if (mrop.getSource() != mem)
           return fail;
         reader = mrop;
-      } else if (csop =
-                     dyn_cast<memref::CollapseShapeOp>(use.getOwner())) {
+      } else if (csop = dyn_cast<memref::CollapseShapeOp>(use.getOwner())) {
         // 1.4 catch the case it has collapse shape befor the copy
         if (reader)
           return fail;
@@ -130,7 +129,7 @@ template <typename T> struct MICORewritePattern : public OpRewritePattern<T> {
       } else if (csop) {
         Location loc = mem.getLoc();
         auto new_expand = b.create<memref::ExpandShapeOp>(
-          loc, mem.getType(), realMem, csop.getReassociation());
+            loc, mem.getType(), realMem, csop.getReassociation());
         mem.replaceAllUsesWith(new_expand);
         reader->erase();
         return success();