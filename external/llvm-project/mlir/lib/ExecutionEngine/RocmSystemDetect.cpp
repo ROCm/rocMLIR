0529b287566b18cfe2fe384fddae3801971c9935
SJW <swaters@amd.com>
2022-09-09 23:31:40 +0000

    * cleanup RocmDeviceName interface

--- external/llvm-project/mlir/lib/ExecutionEngine/RocmSystemDetect.cpp
+++ external/llvm-project/mlir/lib/ExecutionEngine/RocmSystemDetect.cpp
@@ -40,8 +40,8 @@ RocmSystemDetect::RocmSystemDetect() {
     hipDeviceProp_t deviceProps;
     herr = hipGetDeviceProperties(&deviceProps, i);
     if (herr == hipSuccess) {
-      RocmDeviceName chip(deviceProps.gcnArchName);
-      if (chip) {
+      RocmDeviceName chip;
+      if (succeeded(chip.parse(deviceProps.gcnArchName))) {
         llvm::StringRef chipName(chip.getChip());
         llvm::StringRef vendor("AMD");
         llvm::StringRef features(chip.getFeatures());
@@ -79,80 +79,3 @@ RocmSystemDetect::RocmSystemDetect() {
     }
   }
 }
-
-/*
-Full list of properties:
-  name = '\000' <repeats 255 times>,
-  totalGlobalMem = 0x3ff000000,
-  sharedMemPerBlock = 0x10000,
-  regsPerBlock = 0x10000,
-  warpSize = 0x20,
-  maxThreadsPerBlock = 0x400,
-  maxThreadsDim = {0x400, 0x400, 0x400},
-  maxGridSize = {0x7fffffff, 0x7fffffff, 0x7fffffff},
-  clockRate = 0x274a98,
-  memoryClockRate = 0xf4240,
-  memoryBusWidth = 0x100,
-  totalConstMem = 0x3ff000000,
-  major = 0xa,
-  minor = 0x3,
-  multiProcessorCount = 0x24,
-  l2CacheSize = 0x400000,
-  maxThreadsPerMultiProcessor = 0x800,
-  computeMode = 0x0,
-  clockInstructionRate = 0xf4240,
-  arch = {
-    hasGlobalInt32Atomics = 0x1,
-    hasGlobalFloatAtomicExch = 0x1,
-    hasSharedInt32Atomics = 0x1,
-    hasSharedFloatAtomicExch = 0x1,
-    hasFloatAtomicAdd = 0x1,
-    hasGlobalInt64Atomics = 0x1,
-    hasSharedInt64Atomics = 0x1,
-    hasDoubles = 0x1,
-    hasWarpVote = 0x1,
-    hasWarpBallot = 0x1,
-    hasWarpShuffle = 0x1,
-    hasFunnelShift = 0x0,
-    hasThreadFenceSystem = 0x1,
-    hasSyncThreadsExt = 0x0,
-    hasSurfaceFuncs = 0x0,
-    has3dGrid = 0x1,
-    hasDynamicParallelism = 0x0
-  },
-  concurrentKernels = 0x1,
-  pciDomainID = 0x0,
-  pciBusID = 0xa,
-  pciDeviceID = 0x0,
-  maxSharedMemoryPerMultiProcessor = 0x10000,
-  isMultiGpuBoard = 0x0,
-  canMapHostMemory = 0x1,
-  gcnArch = 0x406,
-  gcnArchName = "gfx1030", '\000' <repeats 248 times>,
-  integrated = 0x0,
-  cooperativeLaunch = 0x0,
-  cooperativeMultiDeviceLaunch = 0x0,
-  maxTexture1DLinear = 0xfffffff0,
-  maxTexture1D = 0x4000,
-  maxTexture2D = {0x4000, 0x4000},
-  maxTexture3D = {0x4000, 0x4000, 0x2000},
-  hdpMemFlushCntl = 0x7fffe7672000,
-  hdpRegFlushCntl = 0x7fffe7672004,
-  memPitch = 0x3ff000000,
-  textureAlignment = 0x100,
-  texturePitchAlignment = 0x100,
-  kernelExecTimeoutEnabled = 0x0,
-  ECCEnabled = 0x0,
-  tccDriver = 0x0,
-  cooperativeMultiDeviceUnmatchedFunc = 0x0,
-  cooperativeMultiDeviceUnmatchedGridDim = 0x0,
-  cooperativeMultiDeviceUnmatchedBlockDim = 0x0,
-  cooperativeMultiDeviceUnmatchedSharedMem = 0x0,
-  isLargeBar = 0x0,
-  asicRevision = 0x1,
-  managedMemory = 0x0,
-  directManagedMemAccessFromHost = 0x48,
-  concurrentManagedAccess = 0x0,
-  pageableMemoryAccess = 0x0,
-  pageableMemoryAccessUsesHostPageTables = 0x0
-*/
