98a53827c7ab9612026956d4d37c6bff66660cf7
jungpark-mlir <jungwook.park@amd.com>
2022-10-25 10:26:11 +0100

    Try fixing tablegen error

--- mlir/include/mlir/Dialect/Rock/IR/Rock.h
+++ mlir/include/mlir/Dialect/Rock/IR/Rock.h
@@ -55,7 +55,7 @@ ArrayAttr getIndexArrayAttr(Builder &b, ArrayRef<int64_t> values);
 } // end namespace rock
 } // end namespace mlir
 
-#include "mlir/Dialect/Rock/IR/RockTuningParamAttrInterface.h.inc"
+#include "mlir/Dialect/Rock/IR/RockTuningParamAttrInterface.h"
 #define GET_ATTRDEF_CLASSES
 #include "mlir/Dialect/Rock/IR/RockAttrDefs.h.inc"
 
