950cb3f28b328072271e78203dc3318cd1c1d9b4
Paul Fuqua <pf@acm.org>
2022-11-28 18:31:34 -0600

    Register convert-xmodel-to-gpu.  Has ripples through some CMakeLists.txt files.

--- mlir/include/mlir/InitRocMLIRPasses.h
+++ mlir/include/mlir/InitRocMLIRPasses.h
@@ -15,6 +15,7 @@
 #define MLIR_INITROCMLIRPASSES_H_
 
 // rocMLIR includes
+#include "mlir/Conversion/XModelToGPU/XModelToGPU.h"
 #include "mlir/Conversion/RocMLIRPasses.h"
 #include "mlir/Dialect/MIGraphX/Passes.h"
 #include "mlir/Dialect/Rock/Passes.h"
@@ -50,6 +51,7 @@ inline void registerUpstreamPasses() {
   registerConvertMathToLLVM();
   registerConvertMemRefToLLVM();
   registerConvertTensorToLinalg();
+  registerConvertXModelToGPU();
   registerReconcileUnrealizedCasts();
   registerSCFToControlFlow();
   registerTosaToArith();
