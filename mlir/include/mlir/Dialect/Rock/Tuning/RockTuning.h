055c93c24c4e4716d85c33413ab382215605c7d7
jungpark-mlir <jungwook.park@amd.com>
2022-12-02 19:23:35 +0000

    Update RockTuning.h

--- mlir/include/mlir/Dialect/Rock/Tuning/RockTuning.h
+++ mlir/include/mlir/Dialect/Rock/Tuning/RockTuning.h
@@ -45,8 +45,8 @@ struct ProblemCompare {
       return false;
     // conv case
     if (commonType == KernelType::Conv2D) {
-      Operation lhsOp = dyn_cast<Operation>(lhs);
-      Operation rhsOp = dyn_cast<Operation>(rhs)
+      Operation lhsOp = cast<Operation>(lhs);
+      Operation rhsOp = cast<Operation>(rhs)
       RockConvInterface lhsConv = dyn_cast<RockConvInterface>(lhsOp);
       RockConvInterface rhsConv = dyn_cast<RockConvInterface>(rhsOp);
       if (lhsConv.getFilter().getType() != rhsConv.getFilter().getType())
