0b559efa31705d32831a7ec223d361875eecb392
SJW <swaters@amd.com>
2023-04-26 19:50:38 +0000

    MHAL: initial commit * moved from xmodel * includes mhal.kernel_pkg, mhal.target_obj * Passes:   * mhal-async-graph - converts 'kernel' calls to 'async.launch' (next mhal.launch)   * mhal-target-kernels - create kernel-modules for specified targets   * mhal-package-targets - collect binaries from kernel-modules and moved to mhal.kernel_pkg's   * mhal-select-target - user selection of target kernel binary

---
+++ include/mlir/Dialect/MHAL/IR/MHAL.h
@@ -0,0 +1,57 @@
+//===- MHAL.h - MHAL MLIR Dialect ---------------------*- C++ -*-===//
+//
+// Part of the MLIR Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines MHAL attributes.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_MHAL_IR_MHAL_H_
+#define MLIR_MHAL_IR_MHAL_H_
+
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "mlir/IR/OpDefinition.h"
+
+//===----------------------------------------------------------------------===//
+//  MHAL Dialect
+//===----------------------------------------------------------------------===//
+
+namespace mlir {
+namespace mhal {
+template <typename T>
+class iter_one
+    : public llvm::iterator_facade_base<iter_one<T>, std::forward_iterator_tag,
+                                        T> {
+  T &obj;
+  int idx = 0;
+
+public:
+  iter_one(T &_s, int _idx = 0) : obj(_s), idx(_idx) {}
+  iter_one(const iter_one &) = default;
+  T &operator*() const {
+    assert(idx == 0);
+    return obj;
+  }
+  iter_one &operator++() {
+    ++idx;
+    return *this;
+  }
+  bool operator==(const iter_one &that) const {
+    return obj == that.obj && idx == that.idx;
+  }
+};
+} // namespace mhal
+} // namespace mlir
+
+#include "mlir/Dialect/MHAL/IR/MHALBaseDialect.h.inc"
+#include "mlir/Dialect/MHAL/IR/MHALTypes.h.inc"
+
+#define GET_ATTRDEF_CLASSES
+#include "mlir/Dialect/MHAL/IR/MHALAttrDefs.h.inc"
+
+#endif // MLIR_MHAL_IR_MHAL_H_
---
+++ include/mlir/Dialect/MHAL/Transforms/Passes.h
@@ -0,0 +1,35 @@
+//===- Passes.h - MHAL pass entry points ----------------------*- C++ -*-===//
+//
+// Part of the MLIR Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This header file defines prototypes that expose pass constructors.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_DIALECT_MHAL_PASSES_H_
+#define MLIR_DIALECT_MHAL_PASSES_H_
+
+#include "mlir/Dialect/MHAL/IR/MHAL.h"
+#include "mlir/Pass/Pass.h"
+#include "mlir/Support/LLVM.h"
+#include "llvm/ADT/ArrayRef.h"
+
+namespace mlir {
+namespace mhal {
+
+#define GEN_PASS_DECL_MHALTARGETKERNELSPASS
+#define GEN_PASS_DECL_MHALASYNCGRAPHPASS
+#define GEN_PASS_DECL_MHALPACKAGETARGETSPASS
+#define GEN_PASS_DECL_MHALSELECTTARGETSPASS
+
+#define GEN_PASS_REGISTRATION
+#include "mlir/Dialect/MHAL/Transforms/Passes.h.inc"
+
+} // namespace mhal
+} // namespace mlir
+
+#endif // MLIR_DIALECT_MHAL_PASSES_H_
