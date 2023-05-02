0843e2bf41206909a7acb41011b44db985fcdf72
SJW <swaters@amd.com>
2023-05-02 19:33:46 +0000

    * fixed includes

--- lib/Dialect/MHAL/Transforms/SelectTargets.cpp
+++ lib/Dialect/MHAL/Transforms/SelectTargets.cpp
@@ -10,11 +10,10 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "mlir/Dialect/Async/IR/Async.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "mlir/Dialect/MHAL/IR/MHAL.h"
 #include "mlir/Dialect/MHAL/Transforms/Passes.h"
-#include "mlir/ExecutionEngine/SystemDevices.h"
+#include "mlir/Support/SystemDevice.h"
 #include "mlir/IR/IRMapping.h"
 #include "mlir/IR/Builders.h"
 #include "mlir/IR/PatternMatch.h"
