ROCm/rocMLIR

commit 3cff247d0f8b35b8c8e356b3a02e9a74bea1674f

[mlir] Fix file leak in SeralizeToHsaco (#1257)
SerializetToHsaco, as currently implemented, leaks the file descriptor
of the .hsaco temporary file, which causes issues in long-running
parallel compilation setups.

In addition, move to an LLVM mutex for the linking stage rather than
std::muterx to match upstream better.

Fixes
https://github.com/ROCmSoftwarePlatform/rocMLIR-internal/issues/1151

external/llvm-project/mlir/lib/Dialect/GPU/Transforms/SerializeToHsaco.cpp
@@ -14,6 +14,8 @@
 #include "mlir/Dialect/GPU/Transforms/Passes.h"
 #include "mlir/IR/Location.h"
 #include "mlir/IR/MLIRContext.h"
+#include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Mutex.h"
 
 #if MLIR_GPU_TO_HSACO_PASS_ENABLE
 #include "mlir/ExecutionEngine/OptUtils.h"
@@ -433,10 +435,12 @@ SerializeToHsacoPass::createHsaco(const SmallVectorImpl<char> &isaBinary) {
     return {};
   }
   llvm::FileRemover cleanupHsaco(tempHsacoFilename);
+  // Close here to prevent weird behaviors when LLD opens this file.
+  llvm::sys::fs::closeFile(tempHsacoFD);
 
+  static llvm::sys::Mutex mutex;
   {
-    static std::mutex mutex;
-    const std::lock_guard<std::mutex> lock(mutex);
+    const llvm::sys::ScopedLock lock(mutex);
     // Invoke lld. Expect a true return value from lld.
     if (!lld::elf::link({"ld.lld", "-shared", tempIsaBinaryFilename.c_str(),
                          "-o", tempHsacoFilename.c_str()},
@@ -448,13 +452,14 @@ SerializeToHsacoPass::createHsaco(const SmallVectorImpl<char> &isaBinary) {
   }
 
   // Load the HSA code object.
-  auto hsacoFile = openInputFile(tempHsacoFilename);
+  auto hsacoFile =
+      llvm::MemoryBuffer::getFile(tempHsacoFilename, /*IsText=*/false);
   if (!hsacoFile) {
     emitError(loc, "read HSA code object from temp file error");
     return {};
   }
 
-  StringRef buffer = hsacoFile->getBuffer();
+  StringRef buffer = (*hsacoFile)->getBuffer();
   return std::make_unique<std::vector<char>>(buffer.begin(), buffer.end());
 }
 

commit ab52b0cc64556faa15c4b2f25a7a57a1c3c27624

Fix windows regression caused by 3cff247d0f8b35b8c8e356b3a02e9a74bea1674f
This change caused the following error:
rocMLIR\external\llvm-project\mlir\lib\Dialect\GPU\Transforms\SerializeToHsaco.cpp(439): error C2664: 'std::error_code llvm::sys::fs::closeFile(llvm::sys::fs::file_t &)': cannot convert argument 1 from 'int' to 'llvm::sys::fs::file_t &'

LLVM on Windows uses void* for file_t instead of int. CloseFile accepts this as a parameter and eventually uses it as an int anyway.

The least impactful fix is to initialize a file_t and set the lower int values to the int for the file descriptor. A direct reinterpret_cast from int to void* generates a warning about type sizes since int is 32-bit on Windows.

external/llvm-project/mlir/lib/Dialect/GPU/Transforms/SerializeToHsaco.cpp
@@ -436,7 +436,22 @@ SerializeToHsacoPass::createHsaco(const SmallVectorImpl<char> &isaBinary) {
   }
   llvm::FileRemover cleanupHsaco(tempHsacoFilename);
   // Close here to prevent weird behaviors when LLD opens this file.
+  // file_t is an int on linux and void* on Windows so an explicit cast is
+  // needed. This casting may go from a 32-bit type to a 64-bit type on
+  // Windows which generates a warning so the approach is to just set the low
+  // bytes to the int value.
+  // Internal to closeFile it just uses it as an int again so it should
+  // function.
+  // A proper fix would be to change createTemporaryFile to close the file
+  // after it opens it or to change closeFile to accept an int. Both changes
+  // require changing core LLVM code so this workaround was done.
+#if _WIN32
+  llvm::sys::fs::file_t tempHscacoFilePtr = 0;
+  *reinterpret_cast<int*>(&tempHscacoFilePtr) = tempHsacoFD;
+  llvm::sys::fs::closeFile(tempHscacoFilePtr);
+#else
   llvm::sys::fs::closeFile(tempHsacoFD);
+#endif
 
   static llvm::sys::Mutex mutex;
   {