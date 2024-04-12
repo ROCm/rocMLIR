1f185ae616a6280ce28c0d5835f8d195b1cc3bbb
Giuseppe Rossini <giuseppe.rossini@amd.com>
2024-04-12 08:26:55 +0000

    Formatting

--- mlir/lib/Dialect/Rock/utility/transformMapUtils.cpp
+++ mlir/lib/Dialect/Rock/utility/transformMapUtils.cpp
@@ -360,7 +360,7 @@ void findCountiguousGroupsUnmerge(const ArrayRef<uint32_t> upperDims,
       uint32_t fastestDim = groupCandidate.back();
       size_t fastestDimPosInMerge = dimPosition.back();
       size_t slowestDimPosInMerge = dimPosition.front();
-      for (auto d : groupCandidate){
+      for (auto d : groupCandidate) {
         // Make sure that the dimension cannot be reused
         dimToMerge.erase(d);
         contiguousGroups[keyI.transformPair()].unionSets(fastestDim, d);
@@ -382,7 +382,7 @@ void findCountiguousGroupsUnmerge(const ArrayRef<uint32_t> upperDims,
                thisMergeParams.slice(slowestDimPosInMerge,
                                      fastestDimPosInMerge -
                                          slowestDimPosInMerge))) {
-        if (p == 1 && dimToMerge.contains(d) ){
+        if (p == 1 && dimToMerge.contains(d)) {
           // Make sure that the dimension cannot be reused
           dimToMerge.erase(d);
           contiguousGroups[keyI.transformPair()].unionSets(fastestDim, d);
