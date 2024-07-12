440503a09ae344dc26a925a9e6755da2413ae951
Daniel Hernandez-Juarez <dhernandez0@gmail.com>
2024-07-12 06:59:38 +0000

    First implementation of swizzle, we need to move it to its own pass

--- mlir/lib/Dialect/Rock/Transforms/GridwiseGemmToBlockwise.cpp
+++ mlir/lib/Dialect/Rock/Transforms/GridwiseGemmToBlockwise.cpp
@@ -757,7 +757,7 @@ struct GridwiseGemmRewritePattern : public OpRewritePattern<GridwiseGemmOp> {
                                    ValueRange{gridCoords.g_block,
                                               gridCoords.m_block,
                                               gridCoords.n_block, tid},
-                                   op.getFeatures(), op.getStoreMethod(),
+                                   op.getFeatures(), StoreMethod::Set,
                                    /*forceUnroll=*/true, useIndexDiffs);
     b.eraseOp(op);
 
@@ -2836,25 +2836,179 @@ struct GridwiseGemmAccelRewritePattern
     // Matrix C write out logic.
     Value convertedC = gpuAlloc(b, loc, numOutputVectorElements, destType,
                                 AddressSpace::Private);
-
-    ArrayAttr idToMatrixCMaps =
-        accelEmitterPtr
-            ->computeOutputTransforms(b, loc, M, N, blockSize, bidGridLengths,
-                                      maybeVecDimInfoA->inDPerThread,
-                                      maybeVecDimInfoB->inDPerThread,
-                                      ldsLayoutConfigA.doSwapThreadIterSubDims,
-                                      ldsLayoutConfigB.doSwapThreadIterSubDims)
-            .gridSubTile;
-
     accelEmitterPtr->computeOutputConversion(b, loc, regCAllocOp, convertedC,
-                                             forceUnroll);
+                                            forceUnroll);
 
-    b.create<ThreadwiseWriteAllOp>(
-        loc, convertedC, op.getC(), idToMatrixCMaps,
-        /*extraIndices=*/
-        ValueRange{gridCoords.g_block, gridCoords.m_block, gridCoords.n_block,
-                   tid},
-        op.getFeatures(), op.getStoreMethod(), forceUnroll, useIndexDiffs);
+    if(true) {
+        ArrayAttr idToLDS =
+            accelEmitterPtr
+                ->computeOutputTransforms(b, loc, M, N, blockSize, bidGridLengths,
+                                        maybeVecDimInfoA->inDPerThread,
+                                        maybeVecDimInfoB->inDPerThread,
+                                        ldsLayoutConfigA.doSwapThreadIterSubDims,
+                                        ldsLayoutConfigB.doSwapThreadIterSubDims)
+                .blockSubTile;
+
+        // Allocate LDS for output.
+        auto ldsMemRefOutputType =
+            MemRefType::get({mPerBlock, nPerBlock}, destType, AffineMap{},
+                            workgroupMemoryAddressSpace);
+        auto ldsBufferOutput = b.create<GpuAllocOp>(loc, ldsMemRefOutputType);
+
+        // Emit blockwise stores.
+        b.create<ThreadwiseWriteAllOp>(loc, convertedC, ldsBufferOutput,
+                                       /*extraViews=*/idToLDS,
+                                       /*extraIndices=*/ValueRange{tid},
+                                       op.getFeatures(), StoreMethod::Set,
+                                       /*forceUnroll=*/forceUnroll,
+                                       /*useIndexDiffs=*/useIndexDiffs);
+
+        // Decide register vectorization.
+        constexpr int64_t dimensionM = 1;
+        constexpr int64_t dimensionN = 2;
+        int64_t dataPerThread = (nPerBlock * mPerBlock) / blockSize;
+        VectorizationResult mVectorRes = getMaxVectorization(
+            op.getC(), dimensionM, /*inputDimLen=*/
+            dataPerThread,
+            op.getC().getDefiningOp());
+        int64_t mVectorLen = mVectorRes.max;
+        VectorizationResult nVectorRes = getMaxVectorization(
+            op.getC(), dimensionN, /*inputDimLen=*/
+            dataPerThread,
+            op.getC().getDefiningOp());
+        int64_t nVectorLen = nVectorRes.max;
+
+        int64_t dim, vecLen;
+        if (mVectorLen > nVectorLen) {
+            dim = dimensionM;
+            vecLen = mVectorLen;
+        } else {
+            dim = dimensionN;
+            vecLen = nVectorLen;
+        }
+
+        // LDS barrier.
+        b.create<LDSBarrierOp>(loc);
+        
+        // Load from LDS.
+        int64_t numberOfElementsWrittenPerThread = 128 / destType.getIntOrFloatBitWidth();
+        int64_t iter = dataPerThread / numberOfElementsWrittenPerThread;
+        //int64_t numberOfRepetitionsPerThread = (mPerBlock*nPerBlock) / (blockSize*numberOfElementsWrittenPerThread);
+        //int64_t regsPerThread = numberOfElementsWrittenPerThread*numberOfRepetitionsPerThread;
+        Value finalC = gpuAlloc(b, loc, dataPerThread, destType,
+                                    AddressSpace::Private);
+        
+        TopDownTMBuilder tidIterMerge(b, {"tid", "iter"},
+                                {op.getBlockSize(), dataPerThread});
+        SmallVector<StringRef, 5> throughDims{"tid"};
+        tidIterMerge.passThrough(throughDims);
+        tidIterMerge.merge(
+            {"iter", "numElements"}, {1, 2}, "iter",
+            {iter, numberOfElementsWrittenPerThread});
+        auto tidIterMergeAttr = tidIterMerge.get();
+
+        auto tidIterFlatten = TopDownTMBuilder::below(tidIterMerge, tidIterMergeAttr);
+        tidIterFlatten.unmerge("flattenBlock", 0, {"iter", "tid", "numElements"},
+                            {iter, op.getBlockSize(), numberOfElementsWrittenPerThread});
+        auto tidIterFlattenAttr = tidIterFlatten.get();
+
+        auto flattenToLDS = TopDownTMBuilder::below(tidIterFlatten, tidIterFlattenAttr);
+        if(dim == dimensionN) {
+            flattenToLDS.merge(
+                {"gemmM", "gemmN"}, {0, 1}, "flattenBlock",
+                {mPerBlock, nPerBlock});
+        } else {
+            flattenToLDS.merge(
+                {"gemmN", "gemmM"}, {0, 1}, "flattenBlock",
+                {mPerBlock, nPerBlock});
+        }
+        auto flattenToLDSAttr = flattenToLDS.get();
+
+        SmallVector<Attribute> transformAttrs;
+        transformAttrs.push_back(tidIterMergeAttr);
+        transformAttrs.push_back(tidIterFlattenAttr);
+        transformAttrs.push_back(flattenToLDSAttr);
+        if(dim == dimensionM) {
+            auto switchMandN = TopDownTMBuilder::below(flattenToLDS, flattenToLDSAttr);
+            switchMandN.passThrough({0, 1}, {1, 0});
+            auto switchMandNAttr = switchMandN.get();
+            transformAttrs.push_back(switchMandNAttr);
+        }
+        
+        ArrayAttr ldsRead = b.getArrayAttr(transformAttrs);
+        auto ldsBufferForLoad = transform(b, ldsBufferOutput, ldsRead);
+
+        b.create<ThreadwiseReadIntoOp>(
+            loc, ldsBufferForLoad, finalC, b.getArrayAttr({}),
+            ValueRange{tid}, /*forceUnroll=*/true, /*useIndexDiffs=*/true);
+
+        // Save to memory
+        // Create views as gridwise sub-tile of C
+        TopDownTMBuilder tidIterMerge2(
+            b, {"g_block", "m_block", "n_block", "tid", "iter"},
+            {bidGridLengths[0], bidGridLengths[1], bidGridLengths[2], op.getBlockSize(),
+            dataPerThread},
+            loc);
+        tidIterMerge2.passThrough({"g_block", "m_block", "n_block", "tid"});
+        tidIterMerge2.merge(
+            {"iter", "numElements"}, {4, 5}, "iter",
+            {iter, numberOfElementsWrittenPerThread});
+        auto tidIterMerge2Attr = tidIterMerge2.get();
+
+        TopDownTMBuilder tidIterFlatten2 = TopDownTMBuilder::below(tidIterMerge2, tidIterMerge2Attr);
+        tidIterFlatten2.passThrough({"g_block", "m_block", "n_block"});
+        tidIterFlatten2.unmerge("flattenBlock", 3, {"iter", "tid", "numElements"},
+                            {iter, op.getBlockSize(), numberOfElementsWrittenPerThread});
+        auto tidIterFlatten2Attr = tidIterFlatten2.get();
+                            
+        auto flattenToBlockCoord = TopDownTMBuilder::below(tidIterFlatten2, tidIterFlatten2Attr);
+        flattenToBlockCoord.passThrough({"g_block", "m_block", "n_block"});
+        if(dim == dimensionN) {
+            flattenToBlockCoord.merge(
+                {"block_m", "block_n"}, {3, 4}, "flattenBlock",
+                {mPerBlock, nPerBlock});
+        } else {
+            flattenToBlockCoord.merge(
+                {"block_n", "block_m"}, {3, 4}, "flattenBlock",
+                {nPerBlock, mPerBlock});
+        }
+        TransformMapAttr flattenToBlockCoordAttr = flattenToBlockCoord.get();
+
+        auto toMatrixC = TopDownTMBuilder::below(flattenToBlockCoord, flattenToBlockCoordAttr);
+        toMatrixC.passThrough({"gemmG"}, {0}, {"g_block"});
+        toMatrixC.unmerge("gemmM", 1, {"m_block", "block_m"}, {bidGridLengths[1], mPerBlock});
+        toMatrixC.unmerge("gemmN", 2, {"n_block", "block_n"}, {bidGridLengths[2], nPerBlock});
+        TransformMapAttr toMatrixCAttr = toMatrixC.get();
+
+        SmallVector<Attribute> transformAttrsStore{tidIterMerge2Attr,
+                                            tidIterFlatten2Attr,
+                                            flattenToBlockCoordAttr,
+                                            toMatrixCAttr};
+        ArrayAttr idToMatrixCMaps = b.getArrayAttr(transformAttrsStore);
+
+        b.create<ThreadwiseWriteAllOp>(
+            loc, finalC, op.getC(), idToMatrixCMaps,
+            /*extraIndices=*/
+            ValueRange{gridCoords.g_block, gridCoords.m_block, gridCoords.n_block,
+                    tid},
+            op.getFeatures(), op.getStoreMethod(), forceUnroll, useIndexDiffs);
+    } else {
+        ArrayAttr idToMatrixCMaps =
+            accelEmitterPtr
+                ->computeOutputTransforms(b, loc, M, N, blockSize, bidGridLengths,
+                                        maybeVecDimInfoA->inDPerThread,
+                                        maybeVecDimInfoB->inDPerThread,
+                                        ldsLayoutConfigA.doSwapThreadIterSubDims,
+                                        ldsLayoutConfigB.doSwapThreadIterSubDims)
+                .gridSubTile;
+
+        b.create<ThreadwiseWriteAllOp>(
+            loc, convertedC, op.getC(), idToMatrixCMaps,
+            /*extraIndices=*/
+            ValueRange{gridCoords.g_block, gridCoords.m_block, gridCoords.n_block,
+                    tid},
+            op.getFeatures(), op.getStoreMethod(), forceUnroll, useIndexDiffs);
+    }
     b.eraseOp(op);
     return success();
   }
