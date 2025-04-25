9cbbfa691ca33f63e428b713128291307fe12144
Mirza Halilcevic <mirza.halilcevic@amd.com>
2025-04-25 13:51:59 +0000

    Implement a diagnostic/informational test case.

--- mlir/unittests/Dialect/Rock/AmdArchDbTests.cpp
+++ mlir/unittests/Dialect/Rock/AmdArchDbTests.cpp
@@ -9,11 +9,44 @@
 #include "mlir/Dialect/Rock/utility/AmdArchDb.h"
 
 #include "gtest/gtest.h"
-#include "gtest/internal/gtest-internal.h"
+
+#include "hip/hip_runtime_api.h"
 
 using namespace mlir::rock;
 
-TEST(AmdArchDbTest, NativeArch) {
-  auto info = lookupArchInfo("native:1");
-  EXPECT_EQ(info.totalVGPRPerEU, 512);
+#define SOFT_CHECK_EQ(val1, val2)                                              \
+  do {                                                                         \
+    auto v1 = (val1);                                                          \
+    auto v2 = (val2);                                                          \
+    if (!(v1 == v2)) {                                                         \
+      llvm::errs() << "(SOFT_CHECK_EQ) Expected equality of these values:\n"   \
+                   << "  " #val1 "\n    Which is: " << v1 << "\n"              \
+                   << "  " #val2 "\n    Which is: " << v2 << "\n\n";           \
+    }                                                                          \
+  } while (0)
+
+TEST(AmdArchDbTest, Native) {
+  hipDeviceProp_t prop;
+  if (auto err = hipGetDeviceProperties(&prop, 0); err != hipSuccess) {
+    FAIL() << "hipGetDeviceProperties failed with error: "
+           << hipGetErrorString(err);
+  }
+
+  auto presetInfo = lookupArchInfo(prop.gcnArchName);
+  auto nativeInfo = lookupArchInfo("native");
+
+  SOFT_CHECK_EQ(presetInfo.defaultFeatures, nativeInfo.defaultFeatures);
+  SOFT_CHECK_EQ(presetInfo.waveSize, nativeInfo.waveSize);
+  SOFT_CHECK_EQ(presetInfo.maxWavesPerEU, nativeInfo.maxWavesPerEU);
+  SOFT_CHECK_EQ(presetInfo.totalSGPRPerEU, nativeInfo.totalSGPRPerEU);
+  SOFT_CHECK_EQ(presetInfo.totalVGPRPerEU, nativeInfo.totalVGPRPerEU);
+  SOFT_CHECK_EQ(presetInfo.totalSharedMemPerCU, nativeInfo.totalSharedMemPerCU);
+  SOFT_CHECK_EQ(presetInfo.maxSharedMemPerWG, nativeInfo.maxSharedMemPerWG);
+  SOFT_CHECK_EQ(presetInfo.numEUPerCU, nativeInfo.numEUPerCU);
+  SOFT_CHECK_EQ(presetInfo.minNumCU, nativeInfo.minNumCU);
+  SOFT_CHECK_EQ(presetInfo.hasFp8ConversionInstrs,
+                nativeInfo.hasFp8ConversionInstrs);
+  SOFT_CHECK_EQ(presetInfo.hasOcpFp8ConversionInstrs,
+                nativeInfo.hasOcpFp8ConversionInstrs);
+  SOFT_CHECK_EQ(presetInfo.maxNumXCC, nativeInfo.maxNumXCC);
 }
