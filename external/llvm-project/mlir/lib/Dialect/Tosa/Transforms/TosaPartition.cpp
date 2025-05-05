b54f78e433e6b4f9a9a247b87e6752cc253c52c1
Umang Yadav <umayadav@amd.com>
2025-05-05 22:36:35 +0000

    changes for tosa partition

--- external/llvm-project/mlir/lib/Dialect/Tosa/Transforms/TosaPartition.cpp
+++ external/llvm-project/mlir/lib/Dialect/Tosa/Transforms/TosaPartition.cpp
@@ -150,7 +150,7 @@ bool isLeadingOp(Operation *op, bool trailingOnly) {
 }
 
 bool isTrailingOp(Operation *op) {
-  return isTransposeOp(op) || isFuseableOp(op);
+  return isTransposeOp(op) || isFuseableOp(op) || isConstantZero(op);
 }
 
 class TosaPartitionPass
