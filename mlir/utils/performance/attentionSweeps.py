94e0d7507b1df450e8f6f7c547cee8cbc0814d50
Djordje Antic <djoantic@amd.com>
2025-05-12 06:49:15 -0400

    Implement testAttentionConfig and use AttentionConfiguration from perfRunner instead of inheriting it.

--- mlir/utils/performance/attentionSweeps.py
+++ mlir/utils/performance/attentionSweeps.py
@@ -8,18 +8,23 @@ from dataclasses import dataclass, field
 from typing import Optional, Sequence, Union, Iterable, Callable, Tuple, List, TypeVar
 from enum import Enum
 import sys
-
-from more_itertools import grouper
+import csv
+import random
+import subprocess
+import os
 
 import perfRunner
 from perfRunner import AttentionConfiguration
 from perfRunner import Paths
 from perfRunner import getArch
+from perfRunner import getNumCU
+from perfRunner import create_paths as createPaths
 
 
-class Version(Enum):
-    V2 = 2
-    V3 = 4
+class PerfConfigVersion(Enum):
+    V1 = 'v1'
+    V2 = 'v2'
+    V3 = 'v3'
 
 
 class TestResult(Enum):
@@ -35,56 +40,56 @@ class Options:
     quiet: bool
     arch: str
     flags: list
-    concurrent_tests: int
+    concurrentTests: int
+    numCu: int
+    PerfConfigVersion: PerfConfigVersion
 
 
-@dataclass
-class PerfConfig:
-    config: Sequence[int]
-    version: Version = Version.V3
-    version_map: dict = field(default_factory=lambda: {Version.V2: "v2", Version.V3: "v3"})
+def toAttentionConfig(params, options: Options) -> AttentionConfiguration:
+    shape, perf = params
+    dtype, g, slq, slk, hdqk, hdv, scale, tq, tk, tv, to = shape
+    perfString = f"v2:{','.join(str(x) for x in perf)}"
+    return AttentionConfiguration(dtype, g, slq, slk, hdqk, hdv, scale, tq, tk, tv, to, options.arch, getNumCU(), perfString)
 
-    def __str__(self):
-        suffix = ",".join(str(v) for v in self.config)
-        return f'{self.version_map[self.version]:{suffix}}'
-
-
-@dataclass
-class MLIROnlyAttentionConfig(AttentionConfiguration):
-    attentionConfig: Optional[str] = None  # Optional additional attention configuration
- 
-    def generateMlirDriverCommandLine(self, mlir_gen_flags: Optional[Sequence[str]] = None) -> Sequence[str]:
-        command = [
-            '--operation', 'attention',
-            '-t', self.dataType,
-            '--arch', self.arch,
-            '-g', str(self.g),
-            '-seq_len_q', str(self.seq_len_q),
-            '-seq_len_k', str(self.seq_len_k),
-            '-head_dim_qk', str(self.head_dim_qk),
-            '-head_dim_v', str(self.head_dim_v),
-           # '-with_attn_scale', str(self.valueDim), # TODO: check if needed
-            '-transQ', str(self.transQ),
-            '-transK', str(self.transK),
-            '-transV', str(self.transV),
-            '-transO', str(self.transO),
-        ]
-
-        if mlir_gen_flags:
-            command.extend(mlir_gen_flags)
-
-        if self.perfConfig is not None:
-            command.extend('--perf_config')
-            command.extend(str(self.perfConfig))
-    
-        return command
-
-
-async def testAttentionConfig(config: MLIROnlyAttentionConfig, options: Options, paths: Paths) -> TestResult:
+async def testAttentionConfig(config: AttentionConfiguration, options: Options, paths) -> TestResult:
     """Runs the given configuration and returns whether it successfully concluded,
     failed validation, or was inapplicable."""
     mlirGenOpts = config.generateMlirDriverCommandLine(options.flags)
     mlirGenOpts.append('-pv')
+
+    proc1 = await asyncio.create_subprocess_exec(
+        paths.mlir_paths.rocmlir_gen_path, *mlirGenOpts,
+        stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+
+    proc2 = await asyncio.create_subprocess_exec(
+        paths.mlir_paths.rocmlir_driver_path, '-c',
+        sdin=proc1.stdout, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+
+    proc3 = await asyncio.create_subprocess_exec(
+        paths.mlir_paths.cpu_runner_path,
+        '-02',
+        f'--shared-libs{paths.mlir_paths.libmlir_rocm_runtime_path},'
+        f'{paths.mlir_paths.libconv_validation_wrappers_path},'
+        f'{paths.mlir_paths.libmlir_runtime_utils_path}',
+        '--entry-point-result=void',
+        stdin=proc2.stdout,
+        stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+    )
+
+    stdout3, stderr3 = await proc3.communicate()
+    output = stdout3.decode('utf-8')
+    
+    if proc1.returncode not in [None, 0] or proc2.returncode not in [None, 0]:
+        return TestResult.INVALID
+    if proc3.returncode not in [None, 0]:
+        if options.debug:
+            print("Runner failed:", output)
+            return TestResult.FAILED
+    if 'FAILED' in output or 'nan' in output.lower():
+        return TestResult.FAIL
+    return TestResult.PASS
 
 
 IterType = TypeVar('IterType')
@@ -104,20 +109,18 @@ async def dropGoodConfig(config: AttentionConfiguration,
     result = await testAttentionConfig(config, options, paths)
     if not options.quiet:
         print(f"{result.name}: {config!r}")
-    if result == TestResult.FAIL:
-        return config
-    return result
+    return config if result == TestResult.FAIL else result
 
 
 async def sweepParameters(paramIter: Iterable[IterType],
-        toConfig: Callable[[IterType, Options], MLIROnlyAttentionConfig],
-        options: Options, paths: Paths) -> Tuple[int, int, List[MLIROnlyAttentionConfig]]:
+        toConfig: Callable[[IterType, Options], AttentionConfiguration],
+        options: Options, paths: Paths) -> Tuple[int, int, List[AttentionConfiguration]]:
     failingConfigs = []
     passed = 0
     invalid = 0
     configs = (c for c in (toConfig(p, options) for p in paramIter))
     for configs in grouper((dropGoodConfig(c, options, paths) for c in configs),
-            options.concurrent_tests):
+            options.concurrentTests):
         configsFuture = asyncio.gather(*configs)
         try:
             configsResults = await configsFuture
@@ -134,52 +137,60 @@ async def sweepParameters(paramIter: Iterable[IterType],
 
     return (passed, invalid, failingConfigs)
 
-# TODO: check if relevant for attention parameterSweeps
-def obtainGenFlags(codepath, arch):
-    supportedCodepath = ['mfma', 'vanilla', 'wmma']
-    # If codepath not provided or not supported, infer it from the arch
-    rocmlirGenFlags = []
-    if codepath not in supportedCodepath:
-        if 'gfx908' in arch or 'gfx90a' in arch or 'gfx94' in arch:
-            codepath = 'mfma'
-            rocmlirGenFlags = ['-mfma=on', '-dot=on', '-atomic_add=on', '-atomic_add_f16=on']
-        elif 'gfx95' in arch:
-            codepath = 'mfma'
-            rocmlirGenFlags = ['-mfma=on', '-dot=on', '-atomic_add=on', '-atomic_add_f16=on', '-atomic_add_bf16=on']
-        elif 'gfx906' in arch:
-            codepath = 'vanilla'
-            rocmlirGenFlags = ['-mfma=off', '-dot=on', '-atomic_add=off']
-        elif 'gfx1030' in arch:
-            # Use vanilla codepath for gfx1030 until it has its own perf configs
-            codepath = 'vanilla'
-            rocmlirGenFlags = ['-mfma=off', '-dot=on', '-atomic_add=off']
-        elif 'gfx11' in arch:
-            codepath = 'wmma'
-            rocmlirGenFlags = ['-mfma=off', '-dot=on', '-atomic_add=on', '-wmma=infer']
-        elif 'gfx12' in arch:
-            codepath = 'wmma'
-            rocmlirGenFlags = ['-mfma=off', '-dot=on', '-atomic_add=on', '-wmma=infer', '-atomic_add_f16=on', '-atomic_add_bf16=on']
-        else:
-            # unknow arch info
-            print(f"""Unknown arch {arch}""", file=sys.stderr)
-    
-    return rocmlirGenFlags
+
+def logFailingConfigs(configs: List[AttentionConfiguration], filename: str):
+    with open(filename, mode='w', newLine='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(['CommandLine'])
+        for config in configs:
+            writer.writerow([' '.join(config.generateMlirDriverCommandLine([]))])
 
 
 def main():
     parser = argparse.ArgumentParser(
             description='Sweep parameter values to check correctness of MLIR')
-    parser.add_argument('config',
-        help="The attention configuration to test")
-
-    # TODO: add modes such as debug, quiet, ... 
+    parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--quiet', action='store_true')
+    parser.add_argument('--jobs', type=int, default=4)
+    parser.add_argument('--mlir-build-dir', type=str, required=True)
+    parser.add_argument('--samples', type=int, default=20)
+    parser.add_argument('--log-failures', action='store_true')
+    parser.add_argument('--log-file', type=str, default='failing_configs.csv')
 
     args = parser.parse_args()
-    arch = ','.join(getArch())
 
-    options = Options(debug=args.debug, quiet=args.quiet,
-        arch=arch, flags=obtainGenFlags(args.codepath, arch), concurrent_tests=args.jobs)
-    paths = perfRunner.create_paths(None, args.mlir_build_dir)
+    arch = ','.join(getArch())
+    paths = createPaths(None, args.mlir_build_dir)
+    options = Options(debug=args.debug, quiet=args.quiet, arch=arch, flags=[], concurrent_tests=args.jobs)
+
+    # TODO: define value range
+    dtypes = []
+    groups = []
+    seq_lengths = []
+    head_dims_qk = []
+    head_dims_v = []
+    bools = []
+    # TODO:
+    attention_shapes = list(itertools.product(
+
+    ))
+    # TODO:
+    perf_config_space = list(itertools.product(
+
+
+    ))
+    
+    samples = random.sample(list(itertools.product(attention_shapes, perf_config_space)), args.samples)
+    passed, invalid, failing = asyncio.run(sweepParameters(samples, toAttentionConfig, options, paths))
+
+    print(f"Passed: {passed}, Invalid: {invalid}, Failed: {len(failing)}")
+    if failing:
+        print("\n*** Failing Configurations ***")
+        for fail in failing:
+            print(' '.join(fail.generateMlirDriverCommandLine([])))
+        if args.log_failures:
+            logFailingConfigs(failing, args.log_file)
+
 
 if __name__ == '__main__':
     ret = main()
