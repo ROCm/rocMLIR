5d6d36bcb72af9d032a08ff4de754243f1cf3779
Justin Rosner <justin.rosner@amd.com>
2025-08-18 13:57:24 -0500

    Remove additional line

--- mlir/lib/Dialect/Rock/Generator/ConvGenerator.cpp
+++ mlir/lib/Dialect/Rock/Generator/ConvGenerator.cpp
@@ -835,7 +835,6 @@ LogicalResult ConvGenerator::genConvModule(ModuleOp &module, int rawKernelId,
   // Construct the FuncOp.
   func = func::FuncOp::create(builder.getUnknownLoc(), kernelName, funcType,
                               ArrayRef<NamedAttribute>(kernelAttrs));
-
   // TODO[split-K]: split-K does not work with BwdWeight
   if (!config.disableSplitKForTuning &&
       config.operation.value() != ConvOpType::BwdWeight) {
