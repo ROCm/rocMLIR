da641e5d5bf0871d54ef2a127981a2904743cac8
Justin Rosner <justin.rosner@amd.com>
2025-08-19 22:42:40 +0000

    Add clang-format switches for RUN statement

--- mlir/test/CAPI/reduce_fusible.cpp
+++ mlir/test/CAPI/reduce_fusible.cpp
@@ -1,8 +1,9 @@
 // Check that we can properly use `mlirIsModuleFusible` on ReduceOps without
 // having GemmFeatures set on the func
 
-// RUN: mlir-reduce-fusible-test 2>&1 | FileCheck %s
-// --check-prefix=REDUCE_FUSIBLE
+// clang-format off
+// RUN: mlir-reduce-fusible-test 2>&1 | FileCheck %s --check-prefix=REDUCE_FUSIBLE
+// clang-format on
 
 // REDUCE_FUSIBLE: is fusible: 0
 
@@ -70,23 +71,23 @@ static bool testReduceFusible(MlirContext ctx) {
   // Parse the module from the string
   MlirStringRef moduleStr = mlirStringRefCreateFromCString(mlirModuleText);
   MlirModule moduleOp = mlirModuleCreateParse(ctx, moduleStr);
-
+  
   if (mlirModuleIsNull(moduleOp)) {
     std::cerr << "Failed to parse module" << std::endl;
     return false;
   }
-
+  
   // Create performance configuration string
   std::string perfConfigStr = "v3:64,64,16,32,32,4,4,1,2,1,1";
   MlirStringRef perfStr = mlirStringRefCreateFromCString(perfConfigStr.c_str());
-
+  
   // Test whether the module is fusible
   const bool isFusible = mlirIsModuleFusible(moduleOp, perfStr);
   std::cout << "is fusible: " << isFusible << std::endl;
-
+  
   // Clean up
   mlirModuleDestroy(moduleOp);
-
+  
   return true;
 }
 int main(int argc, char *argv[]) {
@@ -101,10 +102,10 @@ int main(int argc, char *argv[]) {
 
   // Test the module fusibility
   bool isOk = testReduceFusible(ctx);
-
+  
   // Clean up
   mlirContextDestroy(ctx);
-
+  
   if (!isOk) {
     printf("FAILED!\n");
     return 1;
