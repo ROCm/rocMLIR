74f30bd976d2cb9ba5cac3c1659cf12a8fcd52ce
Djordje Antic <djoantic@amd.com>
2025-09-09 19:06:18 +0000

    add test file

---
+++ mlir/utils/performance/dummyfile.py
@@ -0,0 +1,10 @@
+# This is temporary file to check if Github Action will be triggered on PR
+import os
+
+int abc
+
+if abc > 5:
+    print("bigger than 5")
+
+if abc < 5:
+  print("smaller than 5")
