bded5fa36ce01eef6333601f7ae7273b778a21fc
Djordje Antic <djoantic@amd.com>
2025-09-29 13:42:09 +0000

    flake8 issues on mlir: resolved

--- mlir/utils/performance/analysis/testing-metrics.py
+++ mlir/utils/performance/analysis/testing-metrics.py
@@ -2,14 +2,14 @@
 # important metrics (Arithmetic Intensity, Occupancy, Work Imbalance) and
 # plots correlation between them with the selected parameters.
 #
-# Usage: python3 ./testing-metrics.py <debug file(s)> [--n <percent>] [--m <metrics>] [--t <method for threshold>] [--o <output directory>] [--c <numCUs>]
+# Usage: python3 ./testing-metrics.py <debug file(s)> [--n <percent>] [--m <metrics>] [--t <method for threshold>] [--o <output directory>] [--c <num_cus>]
 # Arguments:
 #       <debug file(s)>               Input file(s) in .tsv.debug format
 #       --n <percent>                 Percent of the best perfconfigs to be considered (default=5) - doesn't affect analysis when checking only the best perfConfigs
 #       --m <metrics>                 Metrics to be shown (ai, oc, wi, nmk)
 #       --t <method for threshold>    Method for calculating threshold (m - max, mn - maxN, qn - quantileN)
 #       --o <output directory>        Output directory in case of saving plots
-#       --c <numCUs>                  CUs count if data is not collected on the machine on which the script is executed
+#       --c <num_cus>                  CUs count if data is not collected on the machine on which the script is executed
 
 import pandas as pd
 import seaborn as sns
@@ -21,10 +21,10 @@ from hip import hip
 
 # TODO use AmdArchDb.py (when it's implemented)
 
-numEUPerCU = 4  # may be changed in newer architectures
+num_eu_per_cu = 4  # may be changed in newer architectures
 
 
-def hipCheck(call_result):
+def hip_check(call_result):
     err = call_result[0]
     result = call_result[1:]
     if len(result) == 1:
@@ -34,95 +34,96 @@ def hipCheck(call_result):
     return result
 
 
-def assignNumCu():
+def assign_num_cu():
     if args.c:
         return int(args.c)
     else:
         props = hip.hipDeviceProp_t()
-        hipCheck(hip.hipGetDeviceProperties(props, 0))
+        hip_check(hip.hipGetDeviceProperties(props, 0))
         print(
             "Using info from GPU 0 in your system, the data should have be obtained from the same GPU."
         )
         return int(props.multiProcessorCount)
 
 
-def analyzeGemmFile(file, n):
+def analyze_gemm_file(file, n):
     df = pd.read_csv(file, sep='\t')
 
-    gemmKeys = ['TransA', 'TransB', 'G', 'M', 'K', 'N']
-    perfConfigParams = [
-        'MPerBlock', 'NPerBlock', 'KPerBlock', 'MPerWave', 'NPerWave', 'kPack',
-        'splitKFactor', 'forceUnroll', 'ThreadCopyMore'
+    gemm_keys = ['TransA', 'TransB', 'g', 'm', 'k', 'n']
+    perfconfig_params = [
+        'm_per_block', 'n_per_block', 'KPerBlock', 'MPerWave', 'NPerWave',
+        'kPack', 'split_k_factor', 'forceUnroll', 'ThreadCopyMore'
     ]
 
     assert df["PerfConfig"].str.startswith(
         "v2:").all(), "PerfConfig that doesn't start with v2: found"
-    df[perfConfigParams] = df["PerfConfig"].str.replace("v2:", "").str.split(
+    df[perfconfig_params] = df["PerfConfig"].str.replace("v2:", "").str.split(
         ",", expand=True)
 
     df["ArithmeticIntensity"] = df.apply(
-        lambda row: calculateArithmeticIntensity(row["M"], row["N"], row["K"]),
+        lambda row: calculate_arithmetic_intensity(row["m"], row["n"], row["k"]
+                                                  ),
         axis=1)
-    df["MNPerWave"] = df.apply(lambda row:
-                               (int(row["MPerWave"]) * int(row["NPerWave"])),
+    df["mn_per_wave"] = df.apply(lambda row:
+                                 (int(row["MPerWave"]) * int(row["NPerWave"])),
+                                 axis=1)
+    df["Occupancy"] = df.apply(lambda row: calculate_occupancy(
+        int(row["m"]), int(row["n"]), int(row["g"]), int(row["m_per_block"]),
+        int(row["n_per_block"]), int(row["mn_per_wave"]), min_num_waves),
                                axis=1)
-    df["Occupancy"] = df.apply(lambda row: calculateOccupancy(
-        int(row["M"]), int(row["N"]), int(row["G"]), int(row["MPerBlock"]),
-        int(row["NPerBlock"]), int(row["MNPerWave"]), minNumWaves),
-                               axis=1)
-    df["WorkImbalance"] = df.apply(lambda row: calculateWorkImbalance(
-        int(row["M"]), int(row["N"]), int(row["G"]), int(row["MPerBlock"]),
-        int(row["NPerBlock"]), int(row["MNPerWave"]), minNumWaves,
-        int(row["splitKFactor"])),
+    df["WorkImbalance"] = df.apply(lambda row: calculate_work_imbalance(
+        int(row["m"]), int(row["n"]), int(row["g"]), int(row["m_per_block"]),
+        int(row["n_per_block"]), int(row["mn_per_wave"]), min_num_waves,
+        int(row["split_k_factor"])),
                                    axis=1)
 
-    topList = []
+    top_list = []
 
-    for (key, group) in df.groupby(gemmKeys):
+    for (key, group) in df.groupby(gemm_keys):
         if args.t == "m":
             threshold = group['TFlops'].max()
-            topList.append(group[group['TFlops'] == threshold])
+            top_list.append(group[group['TFlops'] == threshold])
         if args.t == "mn":
             threshold = group[group['TFlops'] >= (group['TFlops'].max() *
                                                   (1 - n / 100))]
-            topList.append(group[group['TFlops'] >= threshold])
+            top_list.append(group[group['TFlops'] >= threshold])
         if args.t == "qn":
             threshold = group['TFlops'].quantile(1 - n / 100.0)
-            topList.append(group[group['TFlops'] >= threshold])
+            top_list.append(group[group['TFlops'] >= threshold])
 
-    list = pd.concat(topList)
+    list = pd.concat(top_list)
 
     df[[
         'Unnamed: 0', 'DataType', 'OutDataType', 'Chip', 'numCU', 'TransA',
-        'TransB', 'G', 'M', 'K', 'N', 'PerfConfig', 'LDSBankConflict',
-        'TFlops', 'MPerBlock', 'NPerBlock', 'KPerBlock', 'MPerWave',
-        'NPerWave', 'kPack', 'splitKFactor', 'forceUnroll', 'ThreadCopyMore',
+        'TransB', 'g', 'm', 'k', 'n', 'PerfConfig', 'LDSBankConflict',
+        'TFlops', 'm_per_block', 'n_per_block', 'KPerBlock', 'MPerWave',
+        'NPerWave', 'kPack', 'split_k_factor', 'forceUnroll', 'ThreadCopyMore',
         'ArithmeticIntensity', 'Occupancy', 'WorkImbalance'
     ]] = df[[
         'Unnamed: 0', 'DataType', 'OutDataType', 'Chip', 'numCU', 'TransA',
-        'TransB', 'G', 'M', 'K', 'N', 'PerfConfig', 'LDSBankConflict',
-        'TFlops', 'MPerBlock', 'NPerBlock', 'KPerBlock', 'MPerWave',
-        'NPerWave', 'kPack', 'splitKFactor', 'forceUnroll', 'ThreadCopyMore',
+        'TransB', 'g', 'm', 'k', 'n', 'PerfConfig', 'LDSBankConflict',
+        'TFlops', 'm_per_block', 'n_per_block', 'KPerBlock', 'MPerWave',
+        'NPerWave', 'kPack', 'split_k_factor', 'forceUnroll', 'ThreadCopyMore',
         'ArithmeticIntensity', 'Occupancy', 'WorkImbalance'
     ]].apply(pd.to_numeric, errors='coerce')
 
     list[[
         'Unnamed: 0', 'DataType', 'OutDataType', 'Chip', 'numCU', 'TransA',
-        'TransB', 'G', 'M', 'K', 'N', 'PerfConfig', 'LDSBankConflict',
-        'TFlops', 'MPerBlock', 'NPerBlock', 'KPerBlock', 'MPerWave',
-        'NPerWave', 'kPack', 'splitKFactor', 'forceUnroll', 'ThreadCopyMore',
+        'TransB', 'g', 'm', 'k', 'n', 'PerfConfig', 'LDSBankConflict',
+        'TFlops', 'm_per_block', 'n_per_block', 'KPerBlock', 'MPerWave',
+        'NPerWave', 'kPack', 'split_k_factor', 'forceUnroll', 'ThreadCopyMore',
         'ArithmeticIntensity', 'Occupancy', 'WorkImbalance'
     ]] = list[[
         'Unnamed: 0', 'DataType', 'OutDataType', 'Chip', 'numCU', 'TransA',
-        'TransB', 'G', 'M', 'K', 'N', 'PerfConfig', 'LDSBankConflict',
-        'TFlops', 'MPerBlock', 'NPerBlock', 'KPerBlock', 'MPerWave',
-        'NPerWave', 'kPack', 'splitKFactor', 'forceUnroll', 'ThreadCopyMore',
+        'TransB', 'g', 'm', 'k', 'n', 'PerfConfig', 'LDSBankConflict',
+        'TFlops', 'm_per_block', 'n_per_block', 'KPerBlock', 'MPerWave',
+        'NPerWave', 'kPack', 'split_k_factor', 'forceUnroll', 'ThreadCopyMore',
         'ArithmeticIntensity', 'Occupancy', 'WorkImbalance'
     ]].apply(pd.to_numeric, errors='coerce')
 
     params = [
-        'MPerBlock', 'NPerBlock', 'KPerBlock', 'MPerWave', 'NPerWave', 'kPack',
-        'splitKFactor'
+        'm_per_block', 'n_per_block', 'KPerBlock', 'MPerWave', 'NPerWave',
+        'kPack', 'split_k_factor'
     ]
 
     if args.m == "ai":
@@ -135,7 +136,7 @@ def analyzeGemmFile(file, n):
             ax.set_ylabel('ArithmeticIntensity')
 
         plt.tight_layout()
-        plotOutput("ArithmeticIntensity_vs_perfConfigParams.png")
+        plot_output("ArithmeticIntensity_vs_perfconfig_params.png")
 
     if args.m == "oc":
         print(list.corr()['Occupancy'])
@@ -147,7 +148,7 @@ def analyzeGemmFile(file, n):
             ax.set_ylabel('Occupancy')
 
         plt.tight_layout()
-        plotOutput("Occupancy_vs_perfConfigParams.png")
+        plot_output("Occupancy_vs_perfconfig_params.png")
 
     if args.m == "wi":
         print(list.corr()['WorkImbalance'])
@@ -159,11 +160,11 @@ def analyzeGemmFile(file, n):
             ax.set_ylabel('WorkImbalance')
 
         plt.tight_layout()
-        plotOutput("WorkImbalance_vs_perfConfigParams.png")
+        plot_output("WorkImbalance_vs_perfconfig_params.png")
 
     if args.m == "nmk":
         figure, axes = plt.subplots(3, 7)
-        for i, nmk in enumerate(['N', 'M', 'K']):
+        for i, nmk in enumerate(['n', 'm', 'k']):
             for j, param in enumerate(params):
                 subplot = axes[i, j]
                 sns.scatterplot(x=list[param],
@@ -172,60 +173,60 @@ def analyzeGemmFile(file, n):
                                 ax=subplot)
                 subplot.set_xlabel(param)
                 subplot.set_ylabel(nmk)
-        plotOutput("NMK_vs_perfConfigParams.png")
+        plot_output("NMK_vs_perfconfig_params.png")
 
-    return pd.concat(topList)
+    return pd.concat(top_list)
 
 
-def analyzeConvFile(file, n):
+def analyze_conv_file(file, n):
     # implementation goes here
 
     raise NotImplementedError(
         "The script is not implemented for analyzing conv files yet.")
 
 
-def calculateArithmeticIntensity(M, N, K):
-    return (M * N * K) / (M * N + M * K + N * K)  # opPerByte/bytesLoaded
+def calculate_arithmetic_intensity(m, n, k):
+    return (m * n * k) / (m * n + m * k + n * k)  # opPerByte/bytesLoaded
 
 
-def calculateOccupancy(M,
-                       N,
-                       G,
-                       MPerBlock,
-                       NPerBlock,
-                       MNPerWave,
-                       minNumWaves,
-                       splitKFactor=1):
-    MTiles = math.ceil(M / MPerBlock)
-    NTiles = math.ceil(N / NPerBlock)
+def calculate_occupancy(m,
+                        n,
+                        g,
+                        m_per_block,
+                        n_per_block,
+                        mn_per_wave,
+                        min_num_waves,
+                        split_k_factor=1):
+    m_tiles = math.ceil(m / m_per_block)
+    n_tiles = math.ceil(n / n_per_block)
 
-    WorkGroups = G * MTiles * NTiles * splitKFactor
-    WavesPerBlock = MPerBlock * NPerBlock // MNPerWave
-    Waves = WorkGroups * WavesPerBlock
+    workgroups = g * m_tiles * n_tiles * split_k_factor
+    waves_per_block = m_per_block * n_per_block // mn_per_wave
+    waves = workgroups * waves_per_block
 
-    return Waves / minNumWaves
-
-
-def calculateWorkImbalance(M,
-                           N,
-                           G,
-                           MPerBlock,
-                           NPerBlock,
-                           MNPerWave,
-                           minNumWaves,
-                           splitKFactor=1):
-    MTiles = math.ceil(M / MPerBlock)
-    NTiles = math.ceil(N / NPerBlock)
-    WorkGroups = G * MTiles * NTiles * splitKFactor
-    WavesPerBlock = MPerBlock * NPerBlock // MNPerWave
-    Waves = WorkGroups * WavesPerBlock
-    WorkImbalanceIntermedResult = (Waves % minNumWaves) / minNumWaves
-
-    return ((1 - (WorkImbalanceIntermedResult))
-            if WorkImbalanceIntermedResult != 0 else 0)
+    return waves / min_num_waves
 
 
-def plotOutput(name):
+def calculate_work_imbalance(m,
+                             n,
+                             g,
+                             m_per_block,
+                             n_per_block,
+                             mn_per_wave,
+                             min_num_waves,
+                             split_k_factor=1):
+    m_tiles = math.ceil(m / m_per_block)
+    n_tiles = math.ceil(n / n_per_block)
+    workgroups = g * m_tiles * n_tiles * split_k_factor
+    waves_per_block = m_per_block * n_per_block // mn_per_wave
+    waves = workgroups * waves_per_block
+    work_imbalance_interm_res = (waves % min_num_waves) / min_num_waves
+
+    return ((1 - (work_imbalance_interm_res))
+            if work_imbalance_interm_res != 0 else 0)
+
+
+def plot_output(name):
     if args.o:
         os.makedirs(args.o, exist_ok=True)
         plt.savefig(os.path.join(args.o, name), dpi=300)
@@ -234,7 +235,7 @@ def plotOutput(name):
         plt.show()
 
 
-def determineFiletype(file):
+def determine_file_type(file):
     with open(file, 'r') as file:
         header = file.readline().strip()
 
@@ -261,19 +262,19 @@ if __name__ == "__main__":
                         default=None)  # Directory in case of saving the plots
     parser.add_argument(
         "--c", type=int, default=None
-    )  # numCUs (if data is not collected on the machine on which the script is executed)
+    )  # num_cus (if data is not collected on the machine on which the script is executed)
 
     args = parser.parse_args()
 
-    numCUs = assignNumCu()
-    minNumWaves = numCUs * numEUPerCU
+    num_cus = assign_num_cu()
+    min_num_waves = num_cus * num_eu_per_cu
 
-    rowList = []
+    row_list = []
 
     for file in args.files:
-        fileType = determineFiletype(file)
+        file_type = determine_file_type(file)
 
-        if fileType == "gemm":
-            rowList.append(analyzeGemmFile(file, args.n))
-        elif fileType == "conv":
-            rowList.append(analyzeConvFile(file, args.n))
+        if file_type == "gemm":
+            row_list.append(analyze_gemm_file(file, args.n))
+        elif file_type == "conv":
+            row_list.append(analyze_conv_file(file, args.n))
