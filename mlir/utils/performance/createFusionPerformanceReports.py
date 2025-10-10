515e53f0d13ce8e54dd0efcb042b7d869f3d2785
Djordje Antic <djoantic@amd.com>
2025-10-10 08:22:18 -0500

    Consistent error message

--- mlir/utils/performance/createFusionPerformanceReports.py
+++ mlir/utils/performance/createFusionPerformanceReports.py
@@ -7,13 +7,15 @@ import numpy as np
 import pandas as pd
 import sys
 
+
 #Create html reports from .csv files
 def printAllPerformance(chip, op):
     perfReportFound = False
 
     COLUMNS_TO_AVERAGE = ['Fusion TFlops', 'MLIR TFlops', 'Fusion/MLIR']
 
-    df = pd.read_csv(chip + '_' + op + '_' + reportUtils.PERF_REPORT_FUSION_FILE)
+    df = pd.read_csv(chip + '_' + op + '_' +
+                     reportUtils.PERF_REPORT_FUSION_FILE)
     perfReportFound = True
 
     plotMean = df[COLUMNS_TO_AVERAGE].agg(reportUtils.geoMean)
@@ -26,19 +28,23 @@ def printAllPerformance(chip, op):
     if (op == 'conv'):
         means = df.groupby(["Direction", "DataType", "InputLayout"])[COLUMNS_TO_AVERAGE]\
             .agg(reportUtils.geoMean)
-        means.loc[("All", "All", "All"),:] = df[COLUMNS_TO_AVERAGE].agg(reportUtils.geoMean)
-        means.to_csv(chip + '_' + op + '_' + reportUtils.PERF_STATS_REPORT_FUSION_FILE)
+        means.loc[("All", "All",
+                   "All"), :] = df[COLUMNS_TO_AVERAGE].agg(reportUtils.geoMean)
+        means.to_csv(chip + '_' + op + '_' +
+                     reportUtils.PERF_STATS_REPORT_FUSION_FILE)
     else:
         means = df.groupby(["DataType"])[COLUMNS_TO_AVERAGE]\
             .agg(reportUtils.geoMean)
         means.loc["All"] = df[COLUMNS_TO_AVERAGE].agg(reportUtils.geoMean)
-        means.to_csv(chip + '_' + op + '_' + reportUtils.PERF_STATS_REPORT_FUSION_FILE)
+        means.to_csv(chip + '_' + op + '_' +
+                     reportUtils.PERF_STATS_REPORT_FUSION_FILE)
 
     toHighlight = ['Fusion/MLIR']
 
     with open(chip + "_" + op + '_' + f"fusion.html", 'w') as htmlOutput:
-        reportUtils.htmlReport(df, means, f"Fusion performance",
-        toHighlight, reportUtils.colorForSpeedups, htmlOutput)
+        reportUtils.htmlReport(df, means, f"Fusion performance", toHighlight,
+                               reportUtils.colorForSpeedups, htmlOutput)
+
 
 # Main function.
 if __name__ == '__main__':
@@ -46,7 +52,7 @@ if __name__ == '__main__':
         printAllPerformance(sys.argv[1], 'conv')
         printAllPerformance(sys.argv[1], 'gemm')
     except FileNotFoundError:
-        print('Perf report not found.')
+        print(f'Error: No performance report found for {sys.argv[1]}')
         sys.exit(1)
     except Exception as e:
         print(f'Error: {e}')
