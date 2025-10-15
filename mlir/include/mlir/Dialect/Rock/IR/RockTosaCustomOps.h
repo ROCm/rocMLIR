02528f60a99329e702f290bd7362958d6b8c517f
Pablo Antonio Martinez <pamartin@amd.com>
2025-10-15 14:05:14 +0200

    Lower 1D and 2D backward convolution ops using tosa::CustomOp (#2016)

---
+++ mlir/include/mlir/Dialect/Rock/IR/RockTosaCustomOps.h
@@ -0,0 +1,15 @@
+#ifndef ROCK_TOSA_CUSTOM_OPS_H
+#define ROCK_TOSA_CUSTOM_OPS_H
+
+// Header to define "domain_name" and "operator_name"s attributes for
+// tosa::CustomOp representing rocMLIR operations.
+
+#define ROCK_CUSTOMOP_DOMAIN_NAME "rocmlir"
+
+// For consistency, this should match ConvOpBwdDataType in RockAttrDefs.td
+#define ROCK_CUSTOMOP_CONV_BWD_DATA "conv_bwd_data"
+#define ROCK_CUSTOMOP_CONV_BWD_WEIGHT "conv_bwd_weight"
+#define ROCK_CUSTOMOP_UNSIGNED_DIV "unsigned_div"
+#define ROCK_CUSTOMOP_UNSIGNED_CAST "unsigned_cast"
+
+#endif // ROCK_TOSA_CUSTOM_OPS_H
