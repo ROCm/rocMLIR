6e9b750d7e9f7f9c02bf180e2122d9ba0170ab24
Daniel Hernandez <danherna@amd.com>
2025-10-23 10:57:20 +0200

    fix

--- mlir/lib/Dialect/Rock/IR/RockDialect.cpp
+++ mlir/lib/Dialect/Rock/IR/RockDialect.cpp
@@ -71,6 +71,18 @@ using namespace mlir::rock;
 //===----------------------------------------------------------------------===//
 // Utility Functions
 //===----------------------------------------------------------------------===//
+
+static Type getElementTypeOrSelfRecursive(Type type) {
+  while (auto shapedType = dyn_cast<ShapedType>(type)) {
+    type = dyn_cast<ShapedType>(type).getElementType();
+  }
+  return type;
+}
+
+static Type getElementTypeOrSelfRecursive(Value val) {
+  return getElementTypeOrSelfRecursive(val.getType());
+}
+
 template <typename OpType>
 static void
 getGemmEffects(OpType &op,
@@ -2089,10 +2101,10 @@ LogicalResult BlockwiseGemmAccelOp::verify() {
   if (loadBFromLDS && !hasB)
     return emitOpError("If loadBFromLDS is enabled, matrixB must be non-null.");
 
-  if (hasA && getElementTypeOrSelf(getMatrixA()) != getElementTypeA())
+  if (hasA && getElementTypeOrSelfRecursive(getMatrixA()) != getElementTypeA())
     return emitOpError("ElementTypeA and matrixA element type don't match");
 
-  if (hasB && getElementTypeOrSelf(getMatrixB()) != getElementTypeB())
+  if (hasB && getElementTypeOrSelfRecursive(getMatrixB()) != getElementTypeB())
     return emitOpError("ElementTypeA and matrixA element type don't match");
 
   return success();
