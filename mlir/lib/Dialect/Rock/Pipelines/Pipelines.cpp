7fe6a6ca1a8b1fcad01c0136fbe59db0333682ba
Pablo Antonio Martinez <pamartin@amd.com>
2025-11-03 14:31:20 +0000

    Pipeline: 1st we add the async op, 2nd we lower it to rocdl

--- mlir/lib/Dialect/Rock/Pipelines/Pipelines.cpp
+++ mlir/lib/Dialect/Rock/Pipelines/Pipelines.cpp
@@ -196,7 +196,7 @@ void rock::buildKernelPipeline(OpPassManager &pm,
     funcPm.addPass(createCanonicalizerPass());
     funcPm.addPass(createConvertLinalgToAffineLoopsPass());
     funcPm.addPass(rock::createRockVectorizeFusionsPass());
-
+    funcPm.addPass(rock::createRockAddAsyncWaitPass());
     // We run reuse LDS before the output swizzle pass because it uses a
     // heuristic to determine whether to swizzle or not, and that heuristic
     // needs the actual LDS usage. After running output swizzle, we'll create a
@@ -295,6 +295,7 @@ void rock::buildBackendPipeline(OpPassManager &pm,
   rocdlOpts.allowedDialects.assign(
       {"memref", "math", "cf", "func", "vector", "arith"});
   gpuPm.addPass(createConvertGpuOpsToROCDLOps(rocdlOpts));
+  gpuPm.addPass(rock::createConvertRockOpsToROCDLOps());
   // Ensure we only run passes on LLVM functions inside GPU modules.
   auto &llvmFuncPm = gpuPm.nest<LLVM::LLVMFuncOp>();
   // -canonicalize -cse so that we don't have to crawl through memref
