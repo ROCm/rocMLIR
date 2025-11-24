a6f4cc1e1a27545c4e38aace339d5f7990389938
Daniel Hernandez <danherna@amd.com>
2025-11-24 14:33:33 +0100

    add test

--- mlir/unittests/Dialect/Rock/InitParamsNonAccelTests.cpp
+++ mlir/unittests/Dialect/Rock/InitParamsNonAccelTests.cpp
@@ -1,4 +1,5 @@
-//===- InitParamsNonAccelTests.cpp - Tests for InitParamsNonAccel --------------===//
+//===- InitParamsNonAccelTests.cpp - Tests for InitParamsNonAccel
+//--------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -19,49 +20,39 @@ namespace {
 //===----------------------------------------------------------------------===//
 
 TEST(V3Config, First) {
-  InitParamsAccel validParams;
-  bool isValidPerfConfig =
-      validParams.deserialize("v3:64,32,32,4,2,4,1,1,2");
+  InitParamsNonAccel validParams;
+  bool isValidPerfConfig = validParams.deserialize("v3:64,32,32,4,2,4,1,1,2");
 
   EXPECT_EQ(isValidPerfConfig, true);
-  EXPECT_EQ(validParams.gemmBThreadCopyMoreGemmKPack, true);
-  EXPECT_EQ(validParams.gemmAThreadCopyMoreGemmK, true);
-  EXPECT_EQ(validParams.getKPack(), 4);
-  EXPECT_EQ(validParams.gemmKPack, 4);
-  EXPECT_EQ(validParams.splitKFactor, 2);
-  EXPECT_EQ(validParams.gemmMPerWave, 32);
-  EXPECT_EQ(validParams.gemmNPerWave, 32);
-  EXPECT_EQ(validParams.gemmNPerWaveOrMnPerXdl, 0);
-  EXPECT_EQ(validParams.gemmScheduleVersion, 3);
-  EXPECT_EQ(validParams.gemmMnPerXdl, 16);
+  EXPECT_EQ(validParams.blockSize, static_cast<uint32_t>(64));
+  EXPECT_EQ(validParams.gemmMPerBlock, 32);
+  EXPECT_EQ(validParams.gemmNPerBlock, 32);
+  EXPECT_EQ(validParams.gemmKPerBlock, 4);
+  EXPECT_EQ(validParams.gemmMPerThread, 2);
+  EXPECT_EQ(validParams.gemmNPerThread, 4);
+  EXPECT_EQ(validParams.splitKFactor, 1);
+  EXPECT_EQ(validParams.gemmScheduleVersion, 1);
   EXPECT_EQ(validParams.outputSwizzle, 2);
-  EXPECT_EQ(validParams.getVersion(), InitParamsAccel::Version::V4);
-  EXPECT_EQ(validParams.gemmMPerBlock, 64);
-  EXPECT_EQ(validParams.gemmNPerBlock, 64);
-  EXPECT_EQ(validParams.gemmKPerBlock, 8);
+  EXPECT_EQ(validParams.getKPack(), 1);
+  EXPECT_EQ(validParams.getVersion(), InitParamsNonAccel::Version::V3);
 }
 
-TEST(V4Config, Second) {
-  InitParamsAccel validParams;
-  bool isValidPerfConfig =
-      validParams.deserialize("v3:64,64,32,4,2,4,1,1,2");
+TEST(V3Config, Second) {
+  InitParamsNonAccel validParams;
+  bool isValidPerfConfig = validParams.deserialize("v3:128,64,32,8,4,2,3,1,2");
 
   EXPECT_EQ(isValidPerfConfig, true);
-  EXPECT_EQ(validParams.gemmBThreadCopyMoreGemmKPack, true);
-  EXPECT_EQ(validParams.gemmAThreadCopyMoreGemmK, false);
-  EXPECT_EQ(validParams.getKPack(), 4);
-  EXPECT_EQ(validParams.gemmKPack, 4);
-  EXPECT_EQ(validParams.splitKFactor, 9);
-  EXPECT_EQ(validParams.gemmMPerWave, 64);
-  EXPECT_EQ(validParams.gemmNPerWave, 32);
-  EXPECT_EQ(validParams.gemmNPerWaveOrMnPerXdl, 0);
-  EXPECT_EQ(validParams.gemmScheduleVersion, 2);
-  EXPECT_EQ(validParams.gemmMnPerXdl, 32);
-  EXPECT_EQ(validParams.outputSwizzle, 2);
-  EXPECT_EQ(validParams.getVersion(), InitParamsAccel::Version::V4);
-  EXPECT_EQ(validParams.gemmMPerBlock, 128);
-  EXPECT_EQ(validParams.gemmNPerBlock, 64);
+  EXPECT_EQ(validParams.blockSize, static_cast<uint32_t>(128));
+  EXPECT_EQ(validParams.gemmMPerBlock, 64);
+  EXPECT_EQ(validParams.gemmNPerBlock, 32);
   EXPECT_EQ(validParams.gemmKPerBlock, 8);
+  EXPECT_EQ(validParams.gemmMPerThread, 4);
+  EXPECT_EQ(validParams.gemmNPerThread, 2);
+  EXPECT_EQ(validParams.splitKFactor, 3);
+  EXPECT_EQ(validParams.gemmScheduleVersion, 1);
+  EXPECT_EQ(validParams.outputSwizzle, 2);
+  EXPECT_EQ(validParams.getKPack(), 1);
+  EXPECT_EQ(validParams.getVersion(), InitParamsNonAccel::Version::V3);
 }
 
 //===----------------------------------------------------------------------===//
@@ -69,7 +60,7 @@ TEST(V4Config, Second) {
 //===----------------------------------------------------------------------===//
 
 TEST(NegativeTests, NoVersion) {
-  InitParamsAccel validParams;
+  InitParamsNonAccel validParams;
   bool isValidPerfConfig =
       validParams.deserialize("128,64,8,64,32,4,9,2,2,0,1");
 
@@ -77,15 +68,14 @@ TEST(NegativeTests, NoVersion) {
 }
 
 TEST(NegativeTests, WrongNumberV3) {
-  InitParamsAccel validParams;
-  bool isValidPerfConfig =
-      validParams.deserialize("v3:64,32,32,4,2,4,1,1");
+  InitParamsNonAccel validParams;
+  bool isValidPerfConfig = validParams.deserialize("v3:64,32,32,4,2,4,1,1");
 
   EXPECT_EQ(isValidPerfConfig, false);
 }
 
 TEST(NegativeTests, Empty) {
-  InitParamsAccel validParams;
+  InitParamsNonAccel validParams;
   bool isValidPerfConfig = validParams.deserialize("");
 
   EXPECT_EQ(isValidPerfConfig, false);
