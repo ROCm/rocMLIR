2d74b6aa951f68eff62209b8e2c6d9317a5067e1
Mirza Halilcevic <mirza.halilcevic@amd.com>
2025-12-16 14:13:33 +0000

    Print all measurements during tuning in debug mode.

--- mlir/utils/performance/tuningRunner.py
+++ mlir/utils/performance/tuningRunner.py
@@ -169,22 +169,30 @@ def get_winning_config(tuning_output, config, paths: Paths, options: Options):
         if options.debug:
             print(result, file=sys.stderr)
         try:
-            # Time is in ns
-            perfconfig, time = result.split('\t')
+            parts = result.split('\t')
+            if len(parts) < 2:
+                print(f"Error parsing tuning driver output line: {result}", file=sys.stderr)
+                continue
+            perfconfig = parts[0]
+            time = parts[-1]
             if time == "N/A":
                 nano_seconds = np.nan
+                measurements = None
             else:
                 nano_seconds = float(time)
+                measurements = parts[1] if len(parts) == 3 else None
         except ValueError:
             print(f"Error parsing tuning driver output line: {result}", file=sys.stderr)
             continue
 
         config.set_perfconfig(perfconfig)
         entry = config.table_entry(nano_seconds)
+        if options.debug:
+            entry["Measurements"] = measurements
         entries.append(entry)
         these_tflops = entry['TFlops']
         # verify that each perfconfig passes accuracy verification
-        if options.verify_perfconfigs:
+        if options.verify_perfconfigs and not np.isnan(nano_seconds):
             try:
                 verify_ns = verify_kernel_with_perfconfig(perfconfig, config, paths, options)
             except TuningError as e:
@@ -238,7 +246,8 @@ def tune_mlir_kernels(configs, conf_class, paths: Paths, options: Options):
 
         tuning_driver_args = [
             f"--tuning-space={options.tuning_space_kind}", f"--num-iterations={MLIR_N_REPEATS}",
-            f"--warmup-iterations={WARMUP_ITERATIONS}", f"--sleep-us={SLEEP_US}", "--use-median"
+            f"--warmup-iterations={WARMUP_ITERATIONS}", f"--sleep-us={SLEEP_US}", "--use-median",
+            f"--show-all-measurements={'true' if options.debug else 'false'}"
         ]
 
         for test_vector in configs:
