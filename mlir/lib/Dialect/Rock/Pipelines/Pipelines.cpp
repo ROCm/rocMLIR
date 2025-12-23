7eb73dbd62fec5527da81b2b86c729d2ad82c3b9
Umang Yadav <umayadav@amd.com>
2025-12-23 20:57:15 +0000

    add schedGroup

--- mlir/lib/Dialect/Rock/Pipelines/Pipelines.cpp
+++ mlir/lib/Dialect/Rock/Pipelines/Pipelines.cpp
@@ -238,6 +238,7 @@ void rock::buildKernelPipeline(OpPassManager &pm,
     funcPm.addPass(
         math::createMathExtendToSupportedTypes(extendToLLVMTypesOptions));
     funcPm.addPass(rock::createRockBufferLoadMergePass());
+    funcPm.addPass(rock::createRockAnalyzeScfForOpsPass());
     funcPm.addPass(rock::createRockTransformToMemrefPass());
     funcPm.addPass(rock::createRockEmulateNarrowTypePass());
     funcPm.addPass(rock::createRockPack4BitGpuOpsTo8BitPass());
