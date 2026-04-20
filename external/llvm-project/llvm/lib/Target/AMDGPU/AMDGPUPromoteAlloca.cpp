d630e7d282ee4f7afe1cc95328933db5762ddc13
Mirza Halilcevic <mirza.halilcevic@amd.com>
2026-04-20 12:43:09 +0000

    Delete outdated TODO comment.

--- external/llvm-project/llvm/lib/Target/AMDGPU/AMDGPUPromoteAlloca.cpp
+++ external/llvm-project/llvm/lib/Target/AMDGPU/AMDGPUPromoteAlloca.cpp
@@ -67,7 +67,6 @@ static cl::opt<unsigned> PromoteAllocaToVectorLimit(
     cl::desc("Maximum byte size to consider promote alloca to vector"),
     cl::init(0));
 
-// TODO(rocmlir): temporary fix for backend bug
 static cl::opt<unsigned> PromoteAllocaToVectorMaxRegs(
     "amdgpu-promote-alloca-to-vector-max-regs",
     cl::desc(
