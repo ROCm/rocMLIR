27b6a1e0bcdf6a7dc1403c8f1aeb51b567aa85d7
Mirza Halilčević <Mirza.Halilcevic@amd.com>
2026-06-03 12:53:27 +0200

    [AIROCMLIR-80] Utilize amd_arch_db python bindings where applicable (#2352)
    * Utilize amd_arch_db python bindings where applicable.
    
    * Address copilot comments.
    
    * Address Cursor code review comments.
    
    * Address code review.
    
    * Fix typo
    
    * Fix formatting.
    
    * Fix comment indent.

--- mlir/utils/performance/analysis/testing-metrics.py
+++ mlir/utils/performance/analysis/testing-metrics.py
@@ -17,33 +17,21 @@ import matplotlib.pyplot as plt
 import argparse
 import math
 import os
-from hip import hip
 
-# TODO use AmdArchDb.py (when it's implemented)
-
-num_eu_per_cu = 4  # may be changed in newer architectures
+from amd_arch_db import lookup_arch_info
 
 
-def hip_check(call_result):
-    err = call_result[0]
-    result = call_result[1:]
-    if len(result) == 1:
-        result = result[0]
-    if isinstance(err, hip.hipError_t) and err != hip.hipError_t.hipSuccess:
-        raise RuntimeError(str(err))
-    return result
+def get_num_eu_per_cu():
+    return lookup_arch_info("native:0").num_eu_per_cu
 
 
 def assign_num_cu():
     if args.c:
         return int(args.c)
-    else:
-        props = hip.hipDeviceProp_t()
-        hip_check(hip.hipGetDeviceProperties(props, 0))
-        print(
-            "Using info from GPU 0 in your system, the data should have be obtained from the same GPU."
-        )
-        return int(props.multiProcessorCount)
+    print(
+        "Using info from GPU 0 in your system, the data should have been obtained from the same GPU."
+    )
+    return lookup_arch_info("native:0").min_num_cu
 
 
 def analyze_gemm_file(file, n):
@@ -244,7 +232,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     num_cus = assign_num_cu()
-    min_num_waves = num_cus * num_eu_per_cu
+    min_num_waves = num_cus * get_num_eu_per_cu()
 
     row_list = []
 
